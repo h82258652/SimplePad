e3b5a48f7d2f53655818cb80e5a51c29e7248932
h82258652 <842053625@qq.com>
2026-03-24 11:12:19 +0800

    u

--- src/SimplePad.UWP.UI/Views/AppMenuBar.xaml.cs
+++ src/SimplePad.UWP.UI/Views/AppMenuBar.xaml.cs
@@ -2,10 +2,12 @@
 using Microsoft.Extensions.DependencyInjection;
 using SimplePad.Core;
 using SimplePad.Settings;
+using Windows.Graphics.Printing;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Printing;
 
 namespace SimplePad.UWP.UI.Views;
 
@@ -19,11 +21,16 @@ public sealed partial class AppMenuBar : UserControl
 
     private readonly IAppSettings _appSettings;
 
+    private PrintDocument? _printDocument;
+    private IPrintDocumentSource? _printDocumentSource;
+
     public AppMenuBar()
     {
         _appSettings = ServiceLocator.Current.GetRequiredService<IAppSettings>();
 
         InitializeComponent();
+
+        PrintMenuItem.Visibility = PrintManager.IsSupported() ? Visibility.Visible : Visibility.Collapsed;
     }
 
     public TextBox? TextBox
@@ -42,9 +49,54 @@ public sealed partial class AppMenuBar : UserControl
         TextBox?.CutSelectionToClipboard();
     }
 
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        if (PrintManager.IsSupported())
+        {
+            PrintManager.GetForCurrentView().PrintTaskRequested += OnPrintTaskRequested;
+        }
+    }
+
     private void OnPasteClick(object sender, RoutedEventArgs e)
     {
         TextBox?.PasteFromClipboard();
+    }
+
+    private async void OnPrintClick(object sender, RoutedEventArgs e)
+    {
+        if (PrintManager.IsSupported())
+        {
+            _printDocument = new PrintDocument();
+            _printDocumentSource = _printDocument.DocumentSource;
+            _printDocument.Paginate += OnPrintDocumentPaginate;
+            _printDocument.GetPreviewPage += OnPrintDocumentGetPreviewPage;
+            _printDocument.AddPages += OnPrintDocumentAddPages;
+
+            _ = await PrintManager.ShowPrintUIAsync();
+        }
+    }
+
+    private void OnPrintDocumentAddPages(object sender, AddPagesEventArgs e)
+    {
+        _printDocument.AddPage(TextBox);
+        _printDocument.AddPagesComplete();
+    }
+
+    private void OnPrintDocumentGetPreviewPage(object sender, GetPreviewPageEventArgs e)
+    {
+        _printDocument.SetPreviewPage(e.PageNumber, TextBox);
+    }
+
+    private void OnPrintDocumentPaginate(object sender, PaginateEventArgs e)
+    {
+    }
+
+    private void OnPrintTaskRequested(PrintManager sender, PrintTaskRequestedEventArgs args)
+    {
+        args.Request.CreatePrintTask("SimplePad", printTaskSourceRequested =>
+        {
+            printTaskSourceRequested.SetSource(_printDocumentSource);
+        });
     }
 
     private async void OnSaveAsClick(object sender, RoutedEventArgs e)
@@ -64,5 +116,13 @@ public sealed partial class AppMenuBar : UserControl
 
     private void OnSaveClick(object sender, RoutedEventArgs e)
     {
+    }
+
+    private void OnUnloaded(object sender, RoutedEventArgs e)
+    {
+        if (PrintManager.IsSupported())
+        {
+            PrintManager.GetForCurrentView().PrintTaskRequested -= OnPrintTaskRequested;
+        }
     }
 }
