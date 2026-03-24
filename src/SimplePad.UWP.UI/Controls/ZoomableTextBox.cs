b5b28b873b4c141f3e3f41e874f9ad9c572c6b49
h82258652 <842053625@qq.com>
2026-03-24 14:40:12 +0800

    u

--- src/SimplePad.UWP.UI/Controls/ZoomableTextBox.cs
+++ src/SimplePad.UWP.UI/Controls/ZoomableTextBox.cs
@@ -9,16 +9,43 @@ public sealed partial class ZoomableTextBox : TextBox
         nameof(ZoomFactor),
         typeof(double),
         typeof(ZoomableTextBox),
-        new PropertyMetadata(1d));
+        new PropertyMetadata(1d, OnZoomFactorChanged));
+
+    private static readonly DependencyProperty ComputedFontSizeProperty = DependencyProperty.Register(
+        nameof(ComputedFontSize),
+        typeof(double),
+        typeof(ZoomableTextBox),
+        new PropertyMetadata(14d));
 
     public ZoomableTextBox()
     {
         DefaultStyleKey = typeof(ZoomableTextBox);
+
+        RegisterPropertyChangedCallback(FontSizeProperty, OnFontSizeChanged);
+
+        ComputedFontSize = FontSize * ZoomFactor;
     }
 
     public double ZoomFactor
     {
         get => (double)GetValue(ZoomFactorProperty);
         set => SetValue(ZoomFactorProperty, value);
+    }
+
+    private double ComputedFontSize
+    {
+        get => (double)GetValue(ComputedFontSizeProperty);
+        set => SetValue(ComputedFontSizeProperty, value);
+    }
+
+    private static void OnZoomFactorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        ZoomableTextBox self = (ZoomableTextBox)d;
+        self.ComputedFontSize = self.FontSize * self.ZoomFactor;
+    }
+
+    private void OnFontSizeChanged(DependencyObject sender, DependencyProperty dp)
+    {
+        ComputedFontSize = FontSize * ZoomFactor;
     }
 }
