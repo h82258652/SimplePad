aead62deb43ae7b4d80c73097eb25a5a143165c0
h82258652 <842053625@qq.com>
2026-03-26 13:23:09 +0800

    u

---
+++ src/SimplePad.UWP.UI/Controls/AppTabViewItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+
+namespace SimplePad.UWP.UI.Controls;
+
+public sealed partial class AppTabViewItem : TabViewItem
+{
+    public static readonly DependencyProperty IsModifiedProperty = DependencyProperty.Register(
+        nameof(IsModified),
+        typeof(bool),
+        typeof(AppTabViewItem),
+        new PropertyMetadata(false));
+
+    public AppTabViewItem()
+    {
+        DefaultStyleKey = typeof(AppTabViewItem);
+        DefaultStyleResourceUri = new Uri("ms-appx:///SimplePad.UWP.UI/Controls/AppTabViewItem.xaml");
+    }
+
+    public bool IsModified
+    {
+        get => (bool)GetValue(IsModifiedProperty);
+        set => SetValue(IsModifiedProperty, value);
+    }
+}
