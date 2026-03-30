c6215ce01f0e510ca49fb65a4e494801fb00387e
h82258652 <842053625@qq.com>
2026-03-30 16:49:46 +0800

    x

---
+++ src/SimplePad.Editor/CursorPosition.cs
@@ -0,0 +1,14 @@
+﻿namespace SimplePad.Editor;
+
+public sealed class CursorPosition
+{
+    public CursorPosition(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public int Column { get; }
+
+    public int Row { get; }
+}
