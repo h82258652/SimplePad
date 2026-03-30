c09e7821fa66e02ef1701063536b36be0341f951
h82258652 <842053625@qq.com>
2026-03-30 16:26:26 +0800

    x

---
+++ src/SimplePad.Themes.UWP/Controls/ThemeContainer.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using SimplePad.Core.UWP.Extensions;
+using SimplePad.Themes.Settings;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SimplePad.Themes.UWP.Controls;
+
+public sealed partial class ThemeContainer : UserControl
+{
+    private readonly IThemeSettings _themeSettings;
+
+    public ThemeContainer()
+    {
+        _themeSettings = ServiceLocator.Current.GetRequiredService<IThemeSettings>();
+
+        UpdateTheme();
+
+        _themeSettings.AppThemeChanged += OnThemeSettingsAppThemeChanged;
+    }
+
+    private async void OnThemeSettingsAppThemeChanged(object? sender, AppTheme e)
+    {
+        await Dispatcher.SafeRunAsync(UpdateTheme);
+    }
+
+    private void UpdateTheme()
+    {
+        RequestedTheme = _themeSettings.AppTheme switch
+        {
+            AppTheme.Light => ElementTheme.Light,
+            AppTheme.Dark => ElementTheme.Dark,
+            _ => ElementTheme.Default
+        };
+    }
+}
---
+++ src/SimplePad.Themes.UWP/Controls/ThemeSettingsControl.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using SimplePad.Core.UWP.Extensions;
+using SimplePad.Themes.Settings;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SimplePad.Themes.UWP.Controls;
+
+public sealed partial class ThemeSettingsControl : UserControl
+{
+    private readonly IThemeSettings _themeSettings;
+
+    public ThemeSettingsControl()
+    {
+        _themeSettings = ServiceLocator.Current.GetRequiredService<IThemeSettings>();
+
+        InitializeComponent();
+
+        UpdateRadioButtons();
+
+        _themeSettings.AppThemeChanged += OnThemeSettingsAppThemeChanged;
+    }
+
+    private void OnDarkThemeRadioButtonChecked(object sender, RoutedEventArgs e)
+    {
+        _themeSettings.AppTheme = AppTheme.Dark;
+    }
+
+    private void OnLightThemeRadioButtonChecked(object sender, RoutedEventArgs e)
+    {
+        _themeSettings.AppTheme = AppTheme.Light;
+    }
+
+    private async void OnThemeSettingsAppThemeChanged(object? sender, AppTheme e)
+    {
+        await Dispatcher.SafeRunAsync(UpdateRadioButtons);
+    }
+
+    private void OnUseSystemSettingsThemeRadioButtonChecked(object sender, RoutedEventArgs e)
+    {
+        _themeSettings.AppTheme = AppTheme.Default;
+    }
+
+    private void UpdateRadioButtons()
+    {
+        switch (_themeSettings.AppTheme)
+        {
+            case AppTheme.Light:
+                LightThemeRadioButton.IsChecked = true;
+                break;
+            case AppTheme.Dark:
+                DarkThemeRadioButton.IsChecked = true;
+                break;
+            default:
+                UseSystemSettingsThemeRadioButton.IsChecked = true;
+                break;
+        }
+    }
+}
