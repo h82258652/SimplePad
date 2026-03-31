df2a6a5ed9b0ae038ebd52736d5fcff508020e7f
h82258652 <842053625@qq.com>
2026-03-31 10:43:12 +0800

    x

---
+++ src/SimplePad.Editor/EditorZoomState.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace SimplePad.Editor;
+
+public sealed class EditorZoomState
+{
+    private const double MaxZoomFactor = 5;
+
+    private const double MinZoomFactor = 0.1;
+
+    private const double ZoomChangeDelta = 0.1;
+
+    private bool _canZoomIn = true;
+    private bool _canZoomOut = true;
+    private double _zoomFactor = 1;
+
+    public event EventHandler<bool>? CanZoomInChanged;
+
+    public event EventHandler<bool>? CanZoomOutChanged;
+
+    public event EventHandler<double>? ZoomFactorChanged;
+
+    public bool CanZoomIn
+    {
+        get => _canZoomIn;
+        private set
+        {
+            if (_canZoomIn != value)
+            {
+                _canZoomIn = value;
+                CanZoomInChanged?.Invoke(this, _canZoomIn);
+            }
+        }
+    }
+
+    public bool CanZoomOut
+    {
+        get => _canZoomOut;
+        private set
+        {
+            if (_canZoomOut != value)
+            {
+                _canZoomOut = value;
+                CanZoomOutChanged?.Invoke(this, _canZoomOut);
+            }
+        }
+    }
+
+    public double ZoomFactor
+    {
+        get
+        {
+            return _zoomFactor;
+        }
+        set
+        {
+            double clampedValue = Math.Clamp(value, MinZoomFactor, MaxZoomFactor);
+            if (_zoomFactor != clampedValue)
+            {
+                _zoomFactor = clampedValue;
+                ZoomFactorChanged?.Invoke(this, _zoomFactor);
+
+                CanZoomIn = _zoomFactor < MaxZoomFactor;
+                CanZoomOut = _zoomFactor > MinZoomFactor;
+            }
+        }
+    }
+
+    public void ResetZoomFactor()
+    {
+        ZoomFactor = 1;
+    }
+
+    public void ZoomIn()
+    {
+        if (CanZoomIn)
+        {
+            ZoomFactor += Math.Min(MaxZoomFactor, ZoomFactor + ZoomChangeDelta);
+        }
+    }
+
+    public void ZoomOut()
+    {
+        if (CanZoomOut)
+        {
+            ZoomFactor -= Math.Max(MinZoomFactor, ZoomFactor - ZoomChangeDelta);
+        }
+    }
+}
--- src/SimplePad.Editor/SimplePadEditorModule.cs
+++ src/SimplePad.Editor/SimplePadEditorModule.cs
@@ -1,7 +1,14 @@
-﻿using SimplePad.Core.Modularity;
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core.Modularity;
 
 namespace SimplePad.Editor;
 
 public sealed class SimplePadEditorModule : AppModuleBase
 {
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        base.ConfigureServices(context);
+
+        context.Services.AddSingleton<EditorZoomState>();
+    }
 }
