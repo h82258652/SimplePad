353b4706fe3c826e5660b936bc0ca9aefc9d5cc8
h82258652 <842053625@qq.com>
2026-03-31 16:18:55 +0800

    x

--- src/SimplePad.MultiTab.UWP/Controls/AppTabViewItem.cs
+++ src/SimplePad.MultiTab.UWP/Controls/AppTabViewItem.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using Microsoft.UI.Xaml.Controls;
 using Windows.UI.Xaml;
 
@@ -14,20 +14,92 @@ public sealed partial class AppTabViewItem : TabViewItem
         typeof(AppTabViewItem),
         new PropertyMetadata(false, OnIsModifiedChanged));
 
+    private const string CommonStatesGroupName = "CommonStates";
+
+    private const string LayoutRootTemplateName = "LayoutRoot";
+
+    private const string PointerOverSelectedStateName = "PointerOverSelected";
+
+    private const string PointerOverStateName = "PointerOver";
+
+    private const string PressedSelectedStateName = "PressedSelected";
+
+    private const string PressedStateName = "Pressed";
+
+    private static readonly DependencyProperty ModifiedIndicatorVisibilityProperty = DependencyProperty.Register(
+        nameof(ModifiedIndicatorVisibility),
+        typeof(Visibility),
+        typeof(AppTabViewItem),
+        new PropertyMetadata(Visibility.Collapsed));
+
+    private VisualStateGroup? _commonStates;
+
     public AppTabViewItem()
     {
         DefaultStyleKey = typeof(AppTabViewItem);
         DefaultStyleResourceUri = new Uri("ms-appx:///SimplePad.MultiTab.UWP/Controls/AppTabViewItem.xaml");
     }
 
-    private static void OnIsModifiedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    {
-        throw new NotImplementedException();
-    }
-
     public bool IsModified
     {
-        get => (bool)GetValue(IsModifiedProperty); 
+        get => (bool)GetValue(IsModifiedProperty);
         set => SetValue(IsModifiedProperty, value);
+    }
+
+    private Visibility ModifiedIndicatorVisibility
+    {
+        get => (Visibility)GetValue(ModifiedIndicatorVisibilityProperty);
+        set => SetValue(ModifiedIndicatorVisibilityProperty, value);
+    }
+
+    protected override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+
+        FrameworkElement layoutRoot = (FrameworkElement)GetTemplateChild(LayoutRootTemplateName);
+        IList<VisualStateGroup> visualStateGroups = VisualStateManager.GetVisualStateGroups(layoutRoot);
+        _commonStates = visualStateGroups.FirstOrDefault(visualStateGroup => visualStateGroup.Name == CommonStatesGroupName);
+        if (_commonStates is not null)
+        {
+            UpdateModifiedIndicatorVisibility();
+
+            _commonStates.CurrentStateChanged -= OnCommonStatesCurrentStateChanged;
+            _commonStates.CurrentStateChanged += OnCommonStatesCurrentStateChanged;
+        }
+    }
+
+    private static void OnIsModifiedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        AppTabViewItem self = (AppTabViewItem)d;
+        self.UpdateModifiedIndicatorVisibility();
+    }
+
+    private void OnCommonStatesCurrentStateChanged(object sender, VisualStateChangedEventArgs e)
+    {
+        UpdateModifiedIndicatorVisibility();
+    }
+
+    private void UpdateModifiedIndicatorVisibility()
+    {
+        if (_commonStates is null)
+        {
+            ModifiedIndicatorVisibility = Visibility.Collapsed;
+            return;
+        }
+
+        if (_commonStates.CurrentState?.Name is PointerOverStateName or PressedStateName or PointerOverSelectedStateName or PressedSelectedStateName)
+        {
+            ModifiedIndicatorVisibility = Visibility.Collapsed;
+            return;
+        }
+
+        if (IsModified)
+        {
+            ModifiedIndicatorVisibility = Visibility.Visible;
+        }
+        else
+        {
+            ModifiedIndicatorVisibility = Visibility.Collapsed;
+        }
     }
 }
