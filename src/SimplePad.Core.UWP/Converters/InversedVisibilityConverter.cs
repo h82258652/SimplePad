7c820b73d203a9c435a8861accfeea169e62b916
h82258652 <842053625@qq.com>
2026-03-31 17:02:41 +0800

    x

---
+++ src/SimplePad.Core.UWP/Converters/InversedVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace SimplePad.Core.UWP.Converters;
+
+public sealed partial class InversedVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        Visibility visibility = (Visibility)value;
+        return visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        Visibility visibility = (Visibility)value;
+        return visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+    }
+}
