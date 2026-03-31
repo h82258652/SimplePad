1833ac9af930a05ed3d3131cb4dfa591b5ff525a
h82258652 <842053625@qq.com>
2026-03-31 11:19:49 +0800

    x

--- src/SimplePad.Editor/SimplePadEditorModule.cs
+++ src/SimplePad.Editor/SimplePadEditorModule.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using SimplePad.Core.Modularity;
+using SimplePad.Fonts;
 
 namespace SimplePad.Editor;
 
 public sealed class SimplePadEditorModule : AppModuleBase
 {
+    public override DependsOn DependModules => DependsOn.Create<SimplePadFontsModule>();
+
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
         base.ConfigureServices(context);
