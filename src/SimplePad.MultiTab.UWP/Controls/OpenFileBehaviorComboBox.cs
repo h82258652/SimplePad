cb660a0bc6551ebff73dbf62801fdc24916f5e95
h82258652 <842053625@qq.com>
2026-03-31 13:28:27 +0800

    x

---
+++ src/SimplePad.MultiTab.UWP/Controls/OpenFileBehaviorComboBox.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using SimplePad.MultiTab.Settings;
+using System;
+using Windows.UI.Xaml.Controls;
+
+namespace SimplePad.MultiTab.UWP.Controls;
+
+public sealed partial class OpenFileBehaviorComboBox : ComboBox
+{
+    private readonly OpenFileBehaviorComboBoxItem[] _items;
+
+    public OpenFileBehaviorComboBox()
+    {
+        _multiTabSettings = ServiceLocator.Current.GetRequiredService<IMultiTabSettings>();
+        _items =
+            [
+            new( OpenFileBehavior.NewTab, ""),
+            new (OpenFileBehavior.NewWindow , ""),
+            ];
+
+        DefaultStyleKey = typeof(OpenFileBehaviorComboBox);
+        DefaultStyleResourceUri = new Uri("ms-appx:///SimplePad.MultiTab.UWP/Controls/OpenFileBehaviorComboBox.xaml");
+
+        _multiTabSettings.OpenFileBehaviorChanged += OnMultiTabSettingsOpenFileBehaviorChanged;
+        SelectionChanged += OnSelectionChanged;
+    }
+
+    private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnMultiTabSettingsOpenFileBehaviorChanged(object? sender, OpenFileBehavior e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private readonly IMultiTabSettings _multiTabSettings;
+}
---
+++ src/SimplePad.MultiTab.UWP/Controls/OpenFileBehaviorComboBoxItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimplePad.MultiTab.UWP.Controls;
+
+internal sealed class OpenFileBehaviorComboBoxItem
+{
+    internal string DisplayName { get; }
+
+    internal OpenFileBehavior Value { get; }
+
+    internal OpenFileBehaviorComboBoxItem(OpenFileBehavior value,  string displayName)
+    {
+        Value = value;
+        DisplayName = displayName;
+    }
+
+    public override string ToString()
+    {
+        return DisplayName;
+    }
+}
