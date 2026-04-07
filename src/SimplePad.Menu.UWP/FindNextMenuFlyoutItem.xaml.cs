a6a109597c140243e1861a497ef314310a159b24
h82258652 <842053625@qq.com>
2026-04-07 13:20:12 +0800

    xx

---
+++ src/SimplePad.Menu.UWP/FindNextMenuFlyoutItem.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SimplePad.Menu;
+
+public sealed partial class FindNextMenuFlyoutItem : MenuFlyoutItem
+{
+    public FindNextMenuFlyoutItem()
+    {
+        InitializeComponent();
+    }
+
+    private void OnClick(object sender, RoutedEventArgs e)
+    {
+    }
+}
---
+++ src/SimplePad.Menu.UWP/FindPreviousMenuFlyoutItem.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SimplePad.Menu;
+
+public sealed partial class FindPreviousMenuFlyoutItem : MenuFlyoutItem
+{
+    public FindPreviousMenuFlyoutItem()
+    {
+        InitializeComponent();
+    }
+
+    private void OnClick(object sender, RoutedEventArgs e)
+    {
+    }
+}
