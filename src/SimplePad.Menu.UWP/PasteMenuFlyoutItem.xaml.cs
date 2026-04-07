7899d702b7d1eaa5935a8f35a37a217fc7a5b6a3
h82258652 <842053625@qq.com>
2026-04-07 11:11:47 +0800

    x

--- src/SimplePad.Menu.UWP/PasteMenuFlyoutItem.xaml.cs
+++ src/SimplePad.Menu.UWP/PasteMenuFlyoutItem.xaml.cs
@@ -1,27 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using SimplePad.Editor;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+namespace SimplePad.Menu;
 
-namespace SimplePad.Menu
+public sealed partial class PasteMenuFlyoutItem : MenuFlyoutItem
 {
-    public sealed partial class PasteMenuFlyoutItem : UserControl
+    public static readonly DependencyProperty TextBoxProperty = DependencyProperty.Register(
+        nameof(TextBox),
+        typeof(IAppTextBox),
+        typeof(PasteMenuFlyoutItem),
+        null);
+
+    public PasteMenuFlyoutItem()
     {
-        public PasteMenuFlyoutItem()
-        {
-            this.InitializeComponent();
-        }
+        InitializeComponent();
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => (IAppTextBox?)GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
+    }
+
+    private void OnClick(object sender, RoutedEventArgs e)
+    {
+        TextBox?.PasteFromClipboard();
     }
 }
