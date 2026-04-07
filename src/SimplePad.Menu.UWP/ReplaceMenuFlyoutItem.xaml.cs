0c726c01a98b532586cba41d0162a35c018a9465
h82258652 <842053625@qq.com>
2026-04-07 12:27:38 +0800

    x

---
+++ src/SimplePad.Menu.UWP/ReplaceMenuFlyoutItem.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SimplePad.Menu;
+
+public sealed partial class ReplaceMenuFlyoutItem : MenuFlyoutItem
+{
+    public ReplaceMenuFlyoutItem()
+    { 
+        InitializeComponent();
+    }
+
+    private void OnClick(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
