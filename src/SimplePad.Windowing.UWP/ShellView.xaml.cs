216cc46c84bfa4dd5a156e7ca9330a42eab459af
h82258652 <842053625@qq.com>
2026-04-07 11:29:08 +0800

    gww

--- src/SimplePad.Windowing.UWP/ShellView.xaml.cs
+++ src/SimplePad.Windowing.UWP/ShellView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml;
+﻿using SimplePad.Settings;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace SimplePad.Windowing;
@@ -7,8 +8,26 @@ public sealed partial class ShellView : UserControl
 {
     public ShellView(IAppWindow appWindow)
     {
+        _settingsState = appWindow.SettingsState;
+
         InitializeComponent();
 
         Window.Current.SetTitleBar(TabView.TitleBar);
+
+        _settingsState.IsVisibleChanged += OnSettingsStateIsVisibleChanged;
+    }
+
+    private readonly SettingsState _settingsState;
+
+    private void OnSettingsStateIsVisibleChanged(object? sender, bool e)
+    {
+        if (_settingsState.IsVisible)
+        {
+
+        }
+        else
+        {
+            Window.Current.SetTitleBar(TabView.TitleBar);
+        }
     }
 }
