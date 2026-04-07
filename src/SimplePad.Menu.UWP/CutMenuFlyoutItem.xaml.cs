f773c010c5ab43c98f61cbe8a4b9b261bb41ff79
h82258652 <842053625@qq.com>
2026-04-07 10:07:15 +0800

    w

--- src/SimplePad.Menu.UWP/CutMenuFlyoutItem.xaml.cs
+++ src/SimplePad.Menu.UWP/CutMenuFlyoutItem.xaml.cs
@@ -1,16 +1,59 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using SimplePad.Editor;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace SimplePad.Menu;
 
 public sealed partial class CutMenuFlyoutItem : MenuFlyoutItem
 {
+    public static readonly DependencyProperty TextBoxProperty = DependencyProperty.Register(
+        nameof(TextBox),
+        typeof(IAppTextBox),
+        typeof(CutMenuFlyoutItem),
+        new PropertyMetadata(null, OnTextBoxChanged));
+
     public CutMenuFlyoutItem()
     {
         InitializeComponent();
     }
 
+    public IAppTextBox? TextBox
+    {
+        get => (IAppTextBox?)GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
+    }
+
+    private static void OnTextBoxChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        CutMenuFlyoutItem self = (CutMenuFlyoutItem)d;
+        IAppTextBox? oldTextBox = (IAppTextBox?)e.OldValue;
+        if (oldTextBox is not null)
+        {
+            oldTextBox.SelectionChanged -= self.OnTextBoxSelectionChanged;
+        }
+
+        IAppTextBox? newTextBox = (IAppTextBox?)e.NewValue;
+        if (newTextBox is not null)
+        {
+            newTextBox.SelectionChanged += self.OnTextBoxSelectionChanged;
+        }
+
+        self.UpdateIsEnabled();
+    }
+
     private void OnClick(object sender, RoutedEventArgs e)
     {
+        TextBox?.CutSelectionToClipboard();
+    }
+
+    private void OnTextBoxSelectionChanged(object? sender, EventArgs e)
+    {
+        UpdateIsEnabled();
+    }
+
+    private void UpdateIsEnabled()
+    {
+        IsEnabled = TextBox is { SelectionLength: > 0 };
     }
 }
--- src/SimplePad.Menu.UWP/DeleteMenuFlyoutItem.xaml.cs
+++ src/SimplePad.Menu.UWP/DeleteMenuFlyoutItem.xaml.cs
@@ -1,16 +1,62 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using SimplePad.Editor;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace SimplePad.Menu;
 
 public sealed partial class DeleteMenuFlyoutItem : MenuFlyoutItem
 {
+    public static readonly DependencyProperty TextBoxProperty = DependencyProperty.Register(
+        nameof(TextBox),
+        typeof(IAppTextBox),
+        typeof(DeleteMenuFlyoutItem),
+        new PropertyMetadata(null, OnTextBoxChanged));
+
     public DeleteMenuFlyoutItem()
     {
         InitializeComponent();
     }
 
+    public IAppTextBox? TextBox
+    {
+        get => (IAppTextBox?)GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
+    }
+
+    private static void OnTextBoxChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        DeleteMenuFlyoutItem self = (DeleteMenuFlyoutItem)d;
+        IAppTextBox? oldTextBox = (IAppTextBox?)e.OldValue;
+        if (oldTextBox is not null)
+        {
+            oldTextBox.SelectionChanged -= self.OnTextBoxSelectionChanged;
+        }
+
+        IAppTextBox? newTextBox = (IAppTextBox?)e.NewValue;
+        if (newTextBox is not null)
+        {
+            newTextBox.SelectionChanged += self.OnTextBoxSelectionChanged;
+        }
+
+        self.UpdateIsEnabled();
+    }
+
     private void OnClick(object sender, RoutedEventArgs e)
     {
+        if (TextBox is { SelectionLength: > 0 } textBox)
+        {
+            textBox.SelectedText = string.Empty;
+        }
+    }
+
+    private void OnTextBoxSelectionChanged(object? sender, EventArgs e)
+    {
+        UpdateIsEnabled();
+    }
+
+    private void UpdateIsEnabled()
+    {
+        IsEnabled = TextBox is { SelectionLength: > 0 };
     }
 }
