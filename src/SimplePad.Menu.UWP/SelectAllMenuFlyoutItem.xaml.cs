093763d2cba3c5ba9ad6bf6315ec60cd3eeac85f
h82258652 <842053625@qq.com>
2026-04-07 11:13:39 +0800

    x

--- src/SimplePad.Menu.UWP/SelectAllMenuFlyoutItem.xaml.cs
+++ src/SimplePad.Menu.UWP/SelectAllMenuFlyoutItem.xaml.cs
@@ -1,16 +1,30 @@
-﻿using Windows.UI.Xaml;
+﻿using SimplePad.Editor;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace SimplePad.Menu;
 
 public sealed partial class SelectAllMenuFlyoutItem : MenuFlyoutItem
 {
+    public static readonly DependencyProperty TextBoxProperty = DependencyProperty.Register(
+        nameof(TextBox),
+        typeof(IAppTextBox),
+        typeof(SelectAllMenuFlyoutItem),
+        null);
+
     public SelectAllMenuFlyoutItem()
     {
         InitializeComponent();
     }
 
+    public IAppTextBox? TextBox
+    {
+        get => (IAppTextBox?)GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
+    }
+
     private void OnClick(object sender, RoutedEventArgs e)
     {
+        TextBox?.SelectAll();
     }
 }
