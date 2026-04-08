9dcd50dfd7a02a2217516609f8dfe2aca9833bd9
h82258652 <842053625@qq.com>
2026-04-08 20:34:59 +0800

    x

--- src/SimplePad.Settings.UWP/SettingsView.xaml.cs
+++ src/SimplePad.Settings.UWP/SettingsView.xaml.cs
@@ -1,11 +1,26 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace SimplePad.Settings;
 
 public sealed partial class SettingsView : UserControl
 {
+    public static readonly DependencyProperty SettingsStateProperty = DependencyProperty.Register(
+        nameof(SettingsState),
+        typeof(SettingsState),
+        typeof(SettingsView),
+        null);
+
     public SettingsView()
     {
         InitializeComponent();
     }
+
+    public SettingsState? SettingsState
+    {
+        get => (SettingsState?)GetValue(SettingsStateProperty);
+        set => SetValue(SettingsStateProperty, value);
+    }
+
+    public UIElement TitleBar => TitleBarElement;
 }
