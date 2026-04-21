c7793ab2afa5638f3ab1dfe621cf8d1373d24442
h82258652 <842053625@qq.com>
2026-04-21 10:42:43 +0800

    x

--- src/SimplePad.Menu.UWP/FindMenuFlyoutItem.xaml.cs
+++ src/SimplePad.Menu.UWP/FindMenuFlyoutItem.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using SimplePad.Core;
+using SimplePad.Editor;
 using SimplePad.Search;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -8,6 +9,12 @@ namespace SimplePad.Menu;
 
 public sealed partial class FindMenuFlyoutItem : MenuFlyoutItem
 {
+    public static readonly DependencyProperty TextBoxProperty = DependencyProperty.Register(
+        nameof(TextBox),
+        typeof(IAppTextBox),
+        typeof(FindMenuFlyoutItem),
+        null);
+
     private readonly SearchViewState _searchViewState;
 
     public FindMenuFlyoutItem()
@@ -17,9 +24,24 @@ public sealed partial class FindMenuFlyoutItem : MenuFlyoutItem
         InitializeComponent();
     }
 
+    public IAppTextBox? TextBox
+    {
+        get => (IAppTextBox?)GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
+    }
+
     private void OnClick(object sender, RoutedEventArgs e)
     {
         _searchViewState.IsVisible = true;
         _searchViewState.IsReplaceMode = false;
+
+        if (TextBox is { } textBox)
+        {
+            string selectedText = textBox.SelectedText;
+            if (!string.IsNullOrEmpty(selectedText))
+            {
+                _searchViewState.SearchText = selectedText;
+            }
+        }
     }
 }
