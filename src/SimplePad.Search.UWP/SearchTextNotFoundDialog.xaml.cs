f73430a9439aec3049620e3a10a036aeade45e4a
h82258652 <842053625@qq.com>
2026-04-21 10:39:59 +0800

    x

---
+++ src/SimplePad.Search.UWP/SearchTextNotFoundDialog.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace SimplePad.Search;
+
+public sealed partial class SearchTextNotFoundDialog : ContentDialog
+{
+    public SearchTextNotFoundDialog(string searchText)
+    {
+        InitializeComponent();
+
+        SearchText.Text = searchText;
+    }
+}
