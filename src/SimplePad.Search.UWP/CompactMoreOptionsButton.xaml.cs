14849d07282f0cc54c13e44d0474f112f5c496ed
h82258652 <842053625@qq.com>
2026-04-22 17:20:11 +0800

    x

---
+++ src/SimplePad.Search.UWP/CompactMoreOptionsButton.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace SimplePad.Search;
+
+public sealed partial class CompactMoreOptionsButton : Button
+{
+    public CompactMoreOptionsButton()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/SimplePad.Search.UWP/IsMatchCaseToggleMenuFlyoutItem.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using SimplePad.Core.Extensions;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SimplePad.Search;
+
+public sealed partial class IsMatchCaseToggleMenuFlyoutItem : ToggleMenuFlyoutItem
+{
+    private readonly CoreDispatcher _dispatcher;
+    private readonly ISearchSettings _searchSettings;
+
+    public IsMatchCaseToggleMenuFlyoutItem()
+    {
+        _dispatcher = Dispatcher;
+        _searchSettings = ServiceLocator.Current.GetRequiredService<ISearchSettings>();
+
+        InitializeComponent();
+
+        UpdateIsChecked();
+
+        _searchSettings.IsMatchCaseChanged += OnSearchSettingsIsMatchCaseChanged;
+    }
+
+    private void OnClick(object sender, RoutedEventArgs e)
+    {
+        _searchSettings.IsMatchCase = IsChecked;
+    }
+
+    private async void OnSearchSettingsIsMatchCaseChanged(object? sender, bool e)
+    {
+        await _dispatcher.SafeRunAsync(UpdateIsChecked);
+    }
+
+    private void UpdateIsChecked()
+    {
+        IsChecked = _searchSettings.IsMatchCase;
+    }
+}
