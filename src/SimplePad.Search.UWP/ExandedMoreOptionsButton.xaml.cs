edb14ef200e3d6ae1ad83aa51128ee2a77939069
h82258652 <842053625@qq.com>
2026-04-27 14:28:32 +0800

    x

---
+++ src/SimplePad.Search.UWP/ExandedMoreOptionsButton.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace SimplePad.Search;
+
+public sealed partial class ExandedMoreOptionsButton : UserControl
+{
+    public ExandedMoreOptionsButton()
+    {
+        InitializeComponent();
+    }
+}
