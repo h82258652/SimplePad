dd2efc919c2911d0867268bf885f941ee9dfd525
h82258652 <842053625@qq.com>
2026-04-28 14:16:06 +0800

    x

--- src/SimplePad.Windowing.UWP/ShellView.xaml.cs
+++ src/SimplePad.Windowing.UWP/ShellView.xaml.cs
@@ -3,16 +3,19 @@ using SimplePad.Core;
 using SimplePad.Settings;
 using SimplePad.Themes;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
 
 namespace SimplePad.Windowing;
 
 public sealed partial class ShellView : ThemeContainer
 {
+    private readonly IAppWindowManager _appWindowManager;
     private readonly SettingsState _settingsState;
 
     public ShellView(IAppWindow appWindow)
     {
         _settingsState = ServiceLocator.Current.GetRequiredService<SettingsState>();
+        _appWindowManager = ServiceLocator.Current.GetRequiredService<IAppWindowManager>();
 
         InitializeComponent();
         TabView.TabRoot = appWindow.TabRoot;
@@ -21,6 +24,13 @@ public sealed partial class ShellView : ThemeContainer
         UpdateTitleBar();
 
         _settingsState.IsVisibleChanged += OnSettingsStateIsVisibleChanged;
+    }
+
+    private async void OnNewWindowKeyboardAcceleratorInvoked(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs args)
+    {
+        args.Handled = true;
+        IAppWindow newAppWindow = await _appWindowManager.ShowNewWindowAsync();
+        newAppWindow.Execute(appWindow => appWindow.TabRoot.AddBlankTab());
     }
 
     private void OnSettingsStateIsVisibleChanged(object? sender, bool e)
