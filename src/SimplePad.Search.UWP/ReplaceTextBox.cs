638da3ccbca2d38c31b225ee9628149aaa950f94
h82258652 <842053625@qq.com>
2026-04-29 17:14:18 +0800

    x

---
+++ src/SimplePad.Search.UWP/ReplaceTextBox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using Windows.UI.Xaml.Controls;
+
+namespace SimplePad.Search;
+
+internal sealed partial class ReplaceTextBox : TextBox
+{
+    private readonly SearchViewState _searchViewState;
+
+    internal ReplaceTextBox()
+    {
+        _searchViewState = ServiceLocator.Current.GetRequiredService<SearchViewState>();
+
+        DefaultStyleKey = typeof(ReplaceTextBox);
+        DefaultStyleResourceUri = new Uri("ms-appx:///SimplePad.Search.UWP/ReplaceTextBox.xaml");
+
+        UpdateText();
+
+        TextChanged += OnTextChanged;
+        _searchViewState.SearchTextChanged += OnSearchViewStateSearchTextChanged;
+    }
+
+    private void OnSearchViewStateSearchTextChanged(object? sender, string e)
+    {
+        UpdateText();
+    }
+
+    private void OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        _searchViewState.ReplaceText = Text;
+    }
+
+    private void UpdateText()
+    {
+        Text = _searchViewState.ReplaceText;
+    }
+}
