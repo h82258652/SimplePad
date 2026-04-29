b190dc5274096429194d12fb86a12653d3357265
h82258652 <842053625@qq.com>
2026-04-29 17:56:51 +0800

    x

---
+++ src/SimplePad.Search.UWP/ReplaceSplitButton.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace SimplePad.Search;
+
+public sealed partial class ReplaceSplitButton : SplitButton
+{
+    public ReplaceSplitButton()
+    {
+        InitializeComponent();
+    }
+}
