c575327344acb190866897658e4b7f08e8c9d45d
h82258652 <842053625@qq.com>
2026-04-30 14:35:42 +0800

    x

--- src/SimplePad.Search.UWP/SearchControl.xaml.cs
+++ src/SimplePad.Search.UWP/SearchControl.xaml.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.Numerics;
+using Microsoft.Extensions.DependencyInjection;
 using SimplePad.Core;
+using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Hosting;
 
 namespace SimplePad.Search;
 
@@ -19,9 +23,38 @@ public sealed partial class SearchControl : UserControl
         InitializeComponent();
         _searchNotificationService.Configure(ShowNotificationFlyout, HideNotificationFlyout, SetNotificationText);
 
-        UpdateVisibility();
+        Visibility = _searchViewState.IsVisible ? Visibility.Visible : Visibility.Collapsed;
 
         _searchViewState.IsVisibleChanged += OnSearchViewStateIsVisibleChanged;
+    }
+
+    private void Hide()
+    {
+        Visual visual = ElementCompositionPreview.GetElementVisual(this);
+        Compositor compositor = visual.Compositor;
+
+        Vector3KeyFrameAnimation scaleAnimation = compositor.CreateVector3KeyFrameAnimation();
+        scaleAnimation.InsertKeyFrame(0f, Vector3.One);
+        scaleAnimation.InsertKeyFrame(1f, Vector3.Zero);
+        scaleAnimation.Duration = TimeSpan.FromSeconds(0.3);
+
+        ScalarKeyFrameAnimation opacityAnimation = compositor.CreateScalarKeyFrameAnimation();
+        opacityAnimation.InsertKeyFrame(0f, 1f);
+        opacityAnimation.InsertKeyFrame(1f, 0f);
+        opacityAnimation.Duration = TimeSpan.FromSeconds(0.3);
+
+        CompositionScopedBatch scopedBatch = compositor.CreateScopedBatch(CompositionBatchTypes.Animation);
+        scopedBatch.Completed += (sender, e) =>
+        {
+            if (_searchViewState.IsVisible)
+            {
+                Visibility = Visibility.Collapsed;
+            }
+        };
+
+        visual.StartAnimation(nameof(visual.Scale), scaleAnimation);
+        visual.StartAnimation(nameof(visual.Opacity), opacityAnimation);
+        scopedBatch.End();
     }
 
     private void HideNotificationFlyout()
@@ -39,6 +72,26 @@ public sealed partial class SearchControl : UserControl
         NotificationText.Text = notificationText;
     }
 
+    private void Show()
+    {
+        Visibility = Visibility.Visible;
+        Visual visual = ElementCompositionPreview.GetElementVisual(this);
+        Compositor compositor = visual.Compositor;
+
+        Vector3KeyFrameAnimation scaleAnimation = compositor.CreateVector3KeyFrameAnimation();
+        scaleAnimation.InsertKeyFrame(0f, Vector3.Zero);
+        scaleAnimation.InsertKeyFrame(1f, Vector3.One);
+        scaleAnimation.Duration = TimeSpan.FromSeconds(0.3);
+
+        ScalarKeyFrameAnimation opacityAnimation = compositor.CreateScalarKeyFrameAnimation();
+        opacityAnimation.InsertKeyFrame(0f, 0f);
+        opacityAnimation.InsertKeyFrame(1f, 1f);
+        opacityAnimation.Duration = TimeSpan.FromSeconds(0.3);
+
+        visual.StartAnimation(nameof(visual.Scale), scaleAnimation);
+        visual.StartAnimation(nameof(visual.Opacity), opacityAnimation);
+    }
+
     private void ShowNotificationFlyout()
     {
         FlyoutBase.ShowAttachedFlyout(RootContainer);
@@ -48,11 +101,11 @@ public sealed partial class SearchControl : UserControl
     {
         if (_searchViewState.IsVisible)
         {
-            Visibility = Visibility.Visible;
+            Show();
         }
         else
         {
-            Visibility = Visibility.Collapsed;
+            Hide();
         }
     }
 }
