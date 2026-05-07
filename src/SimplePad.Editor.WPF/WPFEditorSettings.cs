c03fd475915257e2466702b0411fedef2bb50853
h82258652 <842053625@qq.com>
2026-05-08 00:19:16 +0800

    x

--- src/SimplePad.Editor.WPF/WPFEditorSettings.cs
+++ src/SimplePad.Editor.WPF/WPFEditorSettings.cs
@@ -1,9 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SimplePad.Editor;
 
 internal sealed class WPFEditorSettings : IEditorSettings
 {
+    public bool IsSpellCheckEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public bool IsWordWrap { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public event EventHandler<bool>? IsSpellCheckEnabledChanged;
+    public event EventHandler<bool>? IsWordWrapChanged;
+
+    public Task LoadAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SaveAsync()
+    {
+        throw new NotImplementedException();
+    }
 }
