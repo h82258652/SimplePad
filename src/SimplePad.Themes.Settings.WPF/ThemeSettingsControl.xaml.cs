99e13bd68cceffdfea9254195f0bc367be68396e
h82258652 <842053625@qq.com>
2026-05-07 23:29:50 +0800

    x

---
+++ src/SimplePad.Themes.Settings.WPF/ThemeSettingsControl.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace SimplePad.Themes;
+
+public partial class ThemeSettingsControl : UserControl
+{
+    private readonly Dispatcher _dispatcher;
+    private readonly IThemeSettings _themeSettings;
+
+    public ThemeSettingsControl()
+    {
+        _dispatcher = Dispatcher;
+        _themeSettings = ServiceLocator.Current.GetRequiredService<IThemeSettings>();
+
+        InitializeComponent();
+
+        UpdateRadioButtons();
+
+        _themeSettings.AppThemeChanged += OnThemeSettingsAppThemeChanged;
+    }
+
+    private void OnDarkThemeRadioButtonChecked(object sender, System.Windows.RoutedEventArgs e)
+    {
+        _themeSettings.AppTheme = AppTheme.Dark;
+    }
+
+    private void OnLightThemeRadioButtonChecked(object sender, System.Windows.RoutedEventArgs e)
+    {
+        _themeSettings.AppTheme = AppTheme.Light;
+    }
+
+    private void OnThemeSettingsAppThemeChanged(object? sender, AppTheme e)
+    {
+        _dispatcher.Invoke(UpdateRadioButtons);
+    }
+
+    private void OnUseSystemSettingsThemeRadioButtonChecked(object sender, System.Windows.RoutedEventArgs e)
+    {
+        _themeSettings.AppTheme = AppTheme.Default;
+    }
+
+    private void UpdateRadioButtons()
+    {
+        switch (_themeSettings.AppTheme)
+        {
+            case AppTheme.Light:
+                LightThemeRadioButton.IsChecked = true;
+                break;
+
+            case AppTheme.Dark:
+                DarkThemeRadioButton.IsChecked = true;
+                break;
+
+            default:
+                UseSystemSettingsThemeRadioButton.IsChecked = true;
+                break;
+        }
+    }
+}
---
+++ src/SimplePad.Themes.Settings.WinUI/ThemeSettingsControl.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace SimplePad.Themes;
+
+public sealed partial class ThemeSettingsControl : UserControl
+{
+    public ThemeSettingsControl()
+    {
+        InitializeComponent();
+    }
+}
