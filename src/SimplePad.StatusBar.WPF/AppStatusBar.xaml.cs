66689c41ab88492032272fe79d50c33928a7dbef
h82258652 <842053625@qq.com>
2026-05-07 22:10:00 +0800

    x

---
+++ src/SimplePad.StatusBar.WPF/AppStatusBar.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace SimplePad.StatusBar;
+
+public sealed partial class AppStatusBar : UserControl
+{
+    public AppStatusBar()
+    {
+        InitializeComponent();
+    }
+}
