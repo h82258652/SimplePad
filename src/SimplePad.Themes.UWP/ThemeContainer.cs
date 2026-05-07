f1b88d0eaffa04f5dde29291e14118abca117e80
h82258652 <842053625@qq.com>
2026-05-07 21:38:26 +0800

    x

--- src/SimplePad.Themes.UWP/ThemeContainer.cs
+++ src/SimplePad.Themes.UWP/ThemeContainer.cs
@@ -28,6 +28,6 @@ public partial class ThemeContainer : UserControl
 
     private void UpdateTheme()
     {
-        RequestedTheme = AppThemeUWPExtensions.GetElementTheme(_themeSettings.AppTheme);
+        RequestedTheme = _themeSettings.AppTheme.GetElementTheme();
     }
 }
