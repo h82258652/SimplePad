8e47fd2f70f8b00b60298916641b2d75f86d7cb5
h82258652 <842053625@qq.com>
2026-05-07 22:20:46 +0800

    x

---
+++ src/SimplePad.Fonts.Settings.WPF/PreviewFontTextControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using System;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace SimplePad.Fonts;
+
+public partial class PreviewFontTextControl : UserControl
+{
+    private readonly Dispatcher _dispatcher;
+    private readonly IFontSettings _fontSettings;
+
+    public PreviewFontTextControl()
+    {
+        _dispatcher = Dispatcher;
+        _fontSettings = ServiceLocator.Current.GetRequiredService<IFontSettings>();
+
+        InitializeComponent();
+
+        UpdateFontFamily();
+        UpdateFontStyle();
+        UpdateFontSize();
+    }
+
+    private void UpdateFontFamily()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void UpdateFontSize()
+    {
+        PreviewText.FontSize = _fontSettings.FontSize;
+    }
+
+    private void UpdateFontStyle()
+    {
+        throw new NotImplementedException();
+    }
+}
