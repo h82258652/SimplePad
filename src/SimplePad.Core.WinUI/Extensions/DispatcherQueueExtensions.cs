51aadc12633187bec4d3ec762532f2cc3deb93a9
h82258652 <842053625@qq.com>
2026-05-07 21:06:46 +0800

    x

---
+++ src/SimplePad.Core.WinUI/Extensions/DispatcherQueueExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Dispatching;
+using System.Threading.Tasks;
+
+namespace SimplePad.Core.Extensions;
+
+public static class DispatcherQueueExtensions
+{
+    public static async Task SaveRunAsync(this DispatcherQueue dispatcherQueue, DispatcherQueueHandler callback)
+    {
+        if (dispatcherQueue.HasThreadAccess)
+        {
+            callback();
+        }
+        else
+        {
+            TaskCompletionSource<object?> tcs = new();
+            _ = dispatcherQueue.TryEnqueue(() =>
+            {
+                callback();
+                tcs.SetResult(null);
+            });
+            await tcs.Task;
+        }
+    }
+}
