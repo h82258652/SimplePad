3b8970fe4e54cd9a1d2bf8b32144db63e124c0e3
h82258652 <842053625@qq.com>
2026-05-07 23:32:01 +0800

    x

--- src/SimplePad.Themes.Settings.WinUI/ThemeSettingsControl.xaml.cs
+++ src/SimplePad.Themes.Settings.WinUI/ThemeSettingsControl.xaml.cs
@@ -1,11 +1,64 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using SimplePad.Core;
+using SimplePad.Core.Extensions;
 
 namespace SimplePad.Themes;
 
 public sealed partial class ThemeSettingsControl : UserControl
 {
+    private readonly DispatcherQueue _dispatcherQueue;
+    private readonly IThemeSettings _themeSettings;
+
     public ThemeSettingsControl()
     {
+        _dispatcherQueue = DispatcherQueue;
+        _themeSettings = ServiceLocator.Current.GetRequiredService<IThemeSettings>();
+
         InitializeComponent();
+
+        UpdateRadioButtons();
+
+        _themeSettings.AppThemeChanged += OnThemeSettingsAppThemeChanged;
+    }
+
+    private void OnDarkThemeRadioButtonChecked(object sender, RoutedEventArgs e)
+    {
+        _themeSettings.AppTheme = AppTheme.Dark;
+    }
+
+    private void OnLightThemeRadioButtonChecked(object sender, RoutedEventArgs e)
+    {
+        _themeSettings.AppTheme = AppTheme.Light;
+    }
+
+    private async void OnThemeSettingsAppThemeChanged(object? sender, AppTheme e)
+    {
+        await _dispatcherQueue.SafeRunAsync(UpdateRadioButtons);
+    }
+
+    private void OnUseSystemSettingsThemeRadioButtonChecked(object sender, RoutedEventArgs e)
+    {
+        _themeSettings.AppTheme = AppTheme.Default;
+    }
+
+    private void UpdateRadioButtons()
+    {
+        switch (_themeSettings.AppTheme)
+        {
+            case AppTheme.Light:
+                LightThemeRadioButton.IsChecked = true;
+                break;
+
+            case AppTheme.Dark:
+                DarkThemeRadioButton.IsChecked = true;
+                break;
+
+            default:
+                UseSystemSettingsThemeRadioButton.IsChecked = true;
+                break;
+        }
     }
 }
