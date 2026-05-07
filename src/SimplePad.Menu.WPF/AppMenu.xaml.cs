cbac65bdd7ee0d4d72d573e2ac3f23b22fee98ee
h82258652 <842053625@qq.com>
2026-05-07 23:45:36 +0800

    x

---
+++ src/SimplePad.Menu.WPF/AppMenu.xaml.cs
@@ -0,0 +1,24 @@
+﻿using SimplePad.Editor;
+using System.Windows;
+
+namespace SimplePad.Menu;
+
+public partial class AppMenu : System.Windows.Controls.Menu
+{
+    public static readonly DependencyProperty TextBoxProperty = DependencyProperty.Register(
+        nameof(TextBox),
+        typeof(IAppTextBox),
+        typeof(AppMenu),
+        null);
+
+    public AppMenu()
+    {
+        InitializeComponent();
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => (IAppTextBox?)GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
+    }
+}
