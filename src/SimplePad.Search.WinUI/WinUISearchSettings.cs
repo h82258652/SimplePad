aebb2f0c999b7df53c05698dc8208a3b5f3b68de
h82258652 <842053625@qq.com>
2026-05-07 21:49:03 +0800

    x

---
+++ src/SimplePad.Search.WinUI/WinUISearchSettings.cs
@@ -0,0 +1,26 @@
+﻿using SimplePad.Core.Settings;
+using System;
+using System.Threading.Tasks;
+
+namespace SimplePad.Search;
+
+internal sealed class WinUISearchSettings : AppSettingsBase, ISearchSettings
+{
+    public event EventHandler<bool>? IsMatchCaseChanged;
+
+    public event EventHandler<bool>? IsWrapAroundChanged;
+
+    public bool IsMatchCase { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+   
+    public bool IsWrapAround { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public override Task LoadAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task SaveAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
