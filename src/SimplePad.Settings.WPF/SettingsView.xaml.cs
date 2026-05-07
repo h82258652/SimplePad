c17788ad20a6c9ff86c77179c7af6ba7f3cda06f
h82258652 <842053625@qq.com>
2026-05-07 22:30:05 +0800

    x

---
+++ src/SimplePad.Settings.WPF/SettingsView.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SimplePad.Settings;
+
+public sealed partial class SettingsView : UserControl
+{
+    private readonly SettingsState _settingsState;
+
+    public SettingsView()
+    {
+        _settingsState = ServiceLocator.Current.GetRequiredService<SettingsState>();
+
+        InitializeComponent();
+        // TODO init Version text
+
+        UpdateVisibility();
+        UpdateFontSettingsExpander();
+
+        _settingsState.IsVisibleChanged += OnSettingsStateIsVisibleChanged;
+        _settingsState.IsFontSettingsExpandedChanged += OnSettingsStateIsFontSettingsExpandedChanged;
+    }
+
+    private void OnSettingsStateIsFontSettingsExpandedChanged(object? sender, bool e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnSettingsStateIsVisibleChanged(object? sender, bool e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void UpdateFontSettingsExpander()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void UpdateVisibility()
+    {
+        if (_settingsState is { IsVisible: true })
+        {
+            Visibility = Visibility.Visible;
+        }
+        else
+        {
+            Visibility = Visibility.Collapsed;
+        }
+    }
+}
