9f623ffb5db23d1c474c5e833c8b50057929824a
h82258652 <842053625@qq.com>
2026-05-07 23:51:21 +0800

    x

---
+++ src/SimplePad.Menu.WPF/ZoomInMenuItem.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using SimplePad.Editor;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace SimplePad.Menu;
+
+public partial class ZoomInMenuItem : MenuItem
+{
+    public static readonly DependencyProperty TextBoxProperty = DependencyProperty.Register(
+        nameof(TextBox),
+        typeof(IAppTextBox),
+        typeof(ZoomInMenuItem),
+        null);
+
+    private readonly Dispatcher _dispatcher;
+    private readonly EditorZoomState _editorZoomState;
+
+    public ZoomInMenuItem()
+    {
+        _dispatcher = Dispatcher;
+        _editorZoomState = ServiceLocator.Current.GetRequiredService<EditorZoomState>();
+
+        InitializeComponent();
+
+        UpdateIsEnabled();
+
+        _editorZoomState.CanZoomInChanged += OnEditorZoomStateCanZoomInChanged;
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => (IAppTextBox?)GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
+    }
+
+    private async void OnClick(object sender, RoutedEventArgs e)
+    {
+        _editorZoomState.ZoomIn();
+        await Task.Yield();
+        TextBox?.Focus();
+    }
+
+    private void OnEditorZoomStateCanZoomInChanged(object? sender, bool e)
+    {
+        _dispatcher.Invoke(UpdateIsEnabled);
+    }
+
+    private void UpdateIsEnabled()
+    {
+        IsEnabled = _editorZoomState.CanZoomIn;
+    }
+}
