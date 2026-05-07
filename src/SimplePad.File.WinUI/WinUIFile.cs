8389a62c5fefa15779f4b040d513d9492ee5962c
h82258652 <842053625@qq.com>
2026-05-07 21:30:50 +0800

    x

---
+++ src/SimplePad.File.WinUI/WinUIFile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SimplePad.File;
+
+public sealed class WinUIFile : IFile
+{
+    private LineEndings _lineEndings = LineEndings.CRLF;
+
+    public event EventHandler<LineEndings>? LineEndingsChanged;
+
+    public string FileName => throw new NotImplementedException();
+
+    public LineEndings LineEndings
+    {
+        get => _lineEndings;
+        private set
+        {
+            if (_lineEndings != value)
+            {
+                _lineEndings = value;
+                LineEndingsChanged?.Invoke(this, value);
+            }
+        }
+    }
+
+    public string Path => throw new NotImplementedException();
+
+    public Task<DateTimeOffset> GetModificationTimeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> ReadAllTextAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task WriteAllTextAsync(string text)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/SimplePad.File.WinUI/WinUIFilePickerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SimplePad.File;
+
+internal sealed class WinUIFilePickerService : IFilePickerService
+{
+    public async Task<IFile?> PickOpenFileAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IFile?> PickSaveFileAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
