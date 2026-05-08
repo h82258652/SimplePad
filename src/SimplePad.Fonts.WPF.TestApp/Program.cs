d38e5c0574391810e3418220f5afc4ef807d682b
h82258652 <842053625@qq.com>
2026-05-08 10:11:01 +0800

    x

---
+++ src/SimplePad.Fonts.WPF.TestApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimplePad.Fonts.TestApp;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        App app = new();
+        app.InitializeComponent();
+        app.Run();
+    }
+}
