1aa9dfe80957356cc7ffbe3bed5f411a10197fde
h82258652 <842053625@qq.com>
2026-05-08 14:23:28 +0800

    x

---
+++ src/SimplePad.Fonts.WPF.TestApp/SimplePadFontsWPFTestAppModule.cs
@@ -0,0 +1,8 @@
+﻿using SimplePad.Core.Modularity;
+
+namespace SimplePad.Fonts.TestApp;
+
+public sealed class SimplePadFontsWPFTestAppModule : AppModuleBase
+{
+    public override DependsOn DependModules => DependsOn.Create<SimplePadFontsWPFModule>();
+}
