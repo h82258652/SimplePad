efc662bb44883824717509dc842d84336794679b
h82258652 <842053625@qq.com>
2026-05-11 16:18:27 +0800

    x

--- src/SimplePad.File.WPF/WPFFilePickerService.cs
+++ src/SimplePad.File.WPF/WPFFilePickerService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace SimplePad.File;
 
@@ -7,11 +8,13 @@ internal sealed class WPFFilePickerService : IFilePickerService
 {
     public Task<IFile?> PickOpenFileAsync()
     {
+        OpenFileDialog openFileDialog = new();
         throw new NotImplementedException();
     }
 
     public Task<IFile?> PickSaveFileAsync()
     {
+        SaveFileDialog saveFileDialog = new();
         throw new NotImplementedException();
     }
 }
