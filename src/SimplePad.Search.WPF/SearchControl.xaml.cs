182c8b6c1a7bcfcb55d0a21894ac304a1e31ac40
h82258652 <842053625@qq.com>
2026-05-11 17:07:18 +0800

    x

--- src/SimplePad.Search.WPF/SearchControl.xaml.cs
+++ src/SimplePad.Search.WPF/SearchControl.xaml.cs
@@ -1,11 +1,44 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
 
 namespace SimplePad.Search;
 
 public sealed partial class SearchControl : UserControl
 {
+    private readonly SearchViewState _searchViewState;
+
     public SearchControl()
     {
+        _searchViewState = ServiceLocator.Current.GetRequiredService<SearchViewState>();
+
         InitializeComponent();
+
+        Visibility = _searchViewState.IsVisible ? Visibility.Visible : Visibility.Collapsed;
+
+        _searchViewState.IsVisibleChanged += OnSearchViewStateIsVisibleChanged;
+    }
+
+    private void Hide()
+    {
+        Visibility = Visibility.Collapsed;
+    }
+
+    private void OnSearchViewStateIsVisibleChanged(object? sender, bool e)
+    {
+        if (_searchViewState.IsVisible)
+        {
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
+    }
+
+    private void Show()
+    {
+        Visibility = Visibility.Visible;
     }
 }
