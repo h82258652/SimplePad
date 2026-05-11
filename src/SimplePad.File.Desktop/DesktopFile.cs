c84cb863c4176c5a390c0ca175e46cb688c5e744
h82258652 <842053625@qq.com>
2026-05-11 12:37:40 +0800

    x

---
+++ src/SimplePad.File.Desktop/DesktopFile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SimplePad.File;
+
+public sealed class DesktopFile : IFile
+{
+    private readonly string _path;
+    private LineEndings _lineEndings = LineEndings.CRLF;
+
+    public DesktopFile(string path)
+    {
+        _path = path;
+    }
+
+    public event EventHandler<LineEndings>? LineEndingsChanged;
+
+    public string FileName => throw new NotImplementedException();
+
+    public LineEndings LineEndings
+    {
+        get => _lineEndings;
+        private set
+        {
+            if (_lineEndings != value)
+            {
+                _lineEndings = value;
+                LineEndingsChanged?.Invoke(this, value);
+            }
+        }
+    }
+
+    public string Path => _path;
+
+    public Task<DateTimeOffset> GetModificationTimeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> ReadAllTextAsync()
+    {
+        return System.IO.File.ReadAllTextAsync(_path);
+    }
+
+    public Task WriteAllTextAsync(string text)
+    {
+        return System.IO.File.WriteAllTextAsync(_path, text);
+    }
+}
