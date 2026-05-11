544ab31cad5b05e04c630fdcff9fdef941cd80e9
h82258652 <842053625@qq.com>
2026-05-11 15:25:01 +0800

    x

---
+++ src/SimplePad.Windowing.WPF/WPFServiceProviderIdProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SimplePad.Core;
+
+namespace SimplePad.Windowing;
+
+internal sealed class WPFServiceProviderIdProvider : IServiceProviderIdProvider
+{
+    public int? Get()
+    {
+        throw new NotImplementedException();
+    }
+}
