2730d08052d351266cd4df81b3836825b7714270
h82258652 <842053625@qq.com>
2026-05-12 12:49:01 +0800

    x

---
+++ src/SimplePad.Settings.WPF/SettingsButton.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SimplePad.Settings;
+
+public sealed class SettingsButton : Button
+{
+    private readonly SettingsState _settingsState;
+
+    public SettingsButton()
+    {
+        _settingsState = ServiceLocator.Current.GetRequiredService<SettingsState>();
+
+        Click += OnClick;
+    }
+
+    private void OnClick(object sender, RoutedEventArgs e)
+    {
+        _settingsState.IsVisible = true;
+    }
+}
