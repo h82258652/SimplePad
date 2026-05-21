44ef5d10274c3d19de3dd052dc182325bd75bc52
h82258652 <842053625@qq.com>
2026-05-21 16:37:01 +0800

    x

--- src/SimplePad.Fonts.Settings.WPF/FontStyleComboBox.cs
+++ src/SimplePad.Fonts.Settings.WPF/FontStyleComboBox.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Windows.Controls;
 using System.Windows.Threading;
 using Microsoft.Extensions.DependencyInjection;
--- src/SimplePad.Tabs.Settings.WPF/OpenFileBehaviorComboBox.cs
+++ src/SimplePad.Tabs.Settings.WPF/OpenFileBehaviorComboBox.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Threading;
 using Microsoft.Extensions.DependencyInjection;
 using SimplePad.Core;
 
@@ -6,10 +8,42 @@ namespace SimplePad.Tabs;
 
 public sealed class OpenFileBehaviorComboBox : ComboBox
 {
+    private readonly Dispatcher _dispatcher;
+    private readonly OpenFileBehavior[] _items;
     private readonly ITabsSettings _tabsSettings;
 
     public OpenFileBehaviorComboBox()
     {
-        _tabsSettings = ServiceLocator.Current.GetRequiredService<ITabsSettings>();        
+        _dispatcher = Dispatcher;
+        _tabsSettings = ServiceLocator.Current.GetRequiredService<ITabsSettings>();
+        _items =
+        [
+            OpenFileBehavior.NewTab,
+            OpenFileBehavior.NewWindow
+        ];
+
+        ItemsSource = _items;
+        UpdateSelectedItem();
+
+        _tabsSettings.OpenFileBehaviorChanged += OnTabsSettingsOpenFileBehaviorChanged;
+        SelectionChanged += OnSelectionChanged;
+    }
+
+    private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (SelectedItem is OpenFileBehavior selectedItem)
+        {
+            _tabsSettings.OpenFileBehavior = selectedItem;
+        }
+    }
+
+    private void OnTabsSettingsOpenFileBehaviorChanged(object? sender, OpenFileBehavior e)
+    {
+        _dispatcher.Invoke(UpdateSelectedItem);
+    }
+
+    private void UpdateSelectedItem()
+    {
+        SelectedItem = _items.FirstOrDefault(item => item.Value == _tabsSettings.OpenFileBehavior.Value);
     }
 }
