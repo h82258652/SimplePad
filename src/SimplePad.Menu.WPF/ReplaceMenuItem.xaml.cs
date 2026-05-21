850a78d327869c336d9cfe17463b65cd9c7bed08
h82258652 <842053625@qq.com>
2026-05-21 14:06:22 +0800

    x

---
+++ src/SimplePad.Menu.WPF/ReplaceMenuItem.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace SimplePad.Menu;
+
+public sealed partial class ReplaceMenuItem : MenuItem
+{
+    public ReplaceMenuItem()
+    {
+        InitializeComponent();
+    }
+}
