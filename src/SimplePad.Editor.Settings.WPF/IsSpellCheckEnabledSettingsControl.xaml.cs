3301015f66c397bbb9470de6d8a3796843eecbe2
h82258652 <842053625@qq.com>
2026-05-22 21:10:52 +0800

    x

---
+++ src/SimplePad.Editor.Settings.WPF/IsSpellCheckEnabledSettingsControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace SimplePad.Editor;
+
+public sealed partial class IsSpellCheckEnabledSettingsControl : UserControl
+{
+    private readonly Dispatcher _dispatcher;
+    private readonly IEditorSettings _editorSettings;
+
+    public IsSpellCheckEnabledSettingsControl()
+    {
+        _dispatcher = Dispatcher;
+        _editorSettings = ServiceLocator.Current.GetRequiredService<IEditorSettings>();
+
+        InitializeComponent();
+
+        UpdateIsSpellCheckEnabledToggleSwitch();
+
+        _editorSettings.IsSpellCheckEnabledChanged += OnEditorSettingsIsSpellCheckEnabledChanged;
+    }
+
+    private void OnEditorSettingsIsSpellCheckEnabledChanged(object? sender, bool e)
+    {
+        _dispatcher.Invoke(UpdateIsSpellCheckEnabledToggleSwitch);
+    }
+
+    private void OnIsSpellCheckEnabledToggleSwitchChecked(object sender, RoutedEventArgs e)
+    {
+        _editorSettings.IsSpellCheckEnabled = true;
+    }
+
+    private void OnIsSpellCheckEnabledToggleSwitchUnchecked(object sender, RoutedEventArgs e)
+    {
+        _editorSettings.IsSpellCheckEnabled = false;
+    }
+
+    private void UpdateIsSpellCheckEnabledToggleSwitch()
+    {
+        IsSpellCheckEnabledToggleSwitch.IsChecked = _editorSettings.IsSpellCheckEnabled;
+    }
+}
