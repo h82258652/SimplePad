466ee94199037937501c0b744072260543255026
h82258652 <842053625@qq.com>
2026-06-01 15:54:28 +0800

    x

--- src/SimplePad.Tabs.WPF/AppTabHeaderControl.xaml.cs
+++ src/SimplePad.Tabs.WPF/AppTabHeaderControl.xaml.cs
@@ -1,15 +1,20 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Dragablz;
 
 namespace SimplePad.Tabs;
 
 public sealed partial class AppTabHeaderControl : UserControl
 {
+    private readonly Dispatcher _dispatcher;
+
     public AppTabHeaderControl()
     {
-        InitializeComponent();
+        _dispatcher = Dispatcher;
 
-        UpdateHeaderText();
+        InitializeComponent();
     }
 
     private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
@@ -17,19 +22,47 @@ public sealed partial class AppTabHeaderControl : UserControl
         if (e.OldValue is Tab oldTab)
         {
             oldTab.TitleChanged -= OnTabTitleChanged;
+            oldTab.IsModifiedChanged -= OnTabIsModifiedChanged;
         }
 
         if (e.NewValue is Tab newTab)
         {
             newTab.TitleChanged += OnTabTitleChanged;
+            newTab.IsModifiedChanged += OnTabIsModifiedChanged;
         }
 
         UpdateHeaderText();
+        UpdateDragablzItemIsModified();
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        UpdateDragablzItemIsModified();
+    }
+
+    private void OnTabIsModifiedChanged(object? sender, bool e)
+    {
+        _dispatcher.Invoke(UpdateDragablzItemIsModified);
     }
 
     private void OnTabTitleChanged(object? sender, string e)
     {
         UpdateHeaderText();
+    }
+
+    private void UpdateDragablzItemIsModified()
+    {
+        DependencyObject parent = this;
+        while (parent is not null)
+        {
+            parent = VisualTreeHelper.GetParent(parent);
+
+            if (parent is DragablzItem dragablzItem)
+            {
+                DragablzItemHelper.SetIsModified(dragablzItem, (DataContext as Tab)?.IsModified ?? false);
+                break;
+            }
+        }
     }
 
     private void UpdateHeaderText()
---
+++ src/SimplePad.Tabs.WPF/DragablzItemHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using Dragablz;
+
+namespace SimplePad.Tabs;
+
+internal static class DragablzItemHelper
+{
+    internal static readonly DependencyProperty IsModifiedProperty = DependencyProperty.RegisterAttached(
+        "IsModified",
+        typeof(bool),
+        typeof(DragablzItemHelper),
+        new PropertyMetadata(false));
+
+    internal static bool GetIsModified(DragablzItem obj)
+    {
+        return (bool)obj.GetValue(IsModifiedProperty);
+    }
+
+    internal static void SetIsModified(DragablzItem obj, bool value)
+    {
+        obj.SetValue(IsModifiedProperty, value);
+    }
+}
