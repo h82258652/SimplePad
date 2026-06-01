6cc5c195ccb2a598cfa8ed17439410763231aa76
h82258652 <842053625@qq.com>
2026-06-01 15:16:24 +0800

    x

--- src/SimplePad.Tabs.WPF/AppTabContentControl.xaml.cs
+++ src/SimplePad.Tabs.WPF/AppTabContentControl.xaml.cs
@@ -1,13 +1,50 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using SimplePad.StatusBar;
 
 namespace SimplePad.Tabs;
 
 public sealed partial class AppTabContentControl : UserControl
 {
+    private readonly IStatusBarSettings _statusBarSettings;
+
     public AppTabContentControl()
     {
+        _statusBarSettings = ServiceLocator.Current.GetRequiredService<IStatusBarSettings>();
+
         InitializeComponent();
         AppMenuBar.TextBox = TextBox;
         StatusBar.TextBox = TextBox;
+
+        UpdateStatusBarDividerVisibility();
+
+        _statusBarSettings.IsStatusBarVisibleChanged += OnStatusBarSettingsIsStatusBarVisibleChanged;
+    }
+
+    private void OnStatusBarSettingsIsStatusBarVisibleChanged(object? sender, bool e)
+    {
+        UpdateStatusBarDividerVisibility();
+    }
+
+    private void OnTextBoxTextChanged(object sender, string e)
+    {
+        if (DataContext is Tab tab)
+        {
+            tab.Content = TextBox.Text;
+        }
+    }
+
+    private void UpdateStatusBarDividerVisibility()
+    {
+        if (_statusBarSettings.IsStatusBarVisible)
+        {
+            StatusBarDivider.Visibility = Visibility.Visible;
+        }
+        else
+        {
+            StatusBarDivider.Visibility = Visibility.Collapsed;
+        }
     }
 }
---
+++ src/SimplePad.Tabs.WPF/AppTabHeaderControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SimplePad.Tabs;
+
+public sealed partial class AppTabHeaderControl : UserControl
+{
+    public AppTabHeaderControl()
+    {
+        InitializeComponent();
+
+        UpdateHeaderText();
+    }
+
+    private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        if (e.OldValue is Tab oldTab)
+        {
+            oldTab.TitleChanged -= OnTabTitleChanged;
+        }
+
+        if (e.NewValue is Tab newTab)
+        {
+            newTab.TitleChanged += OnTabTitleChanged;
+        }
+
+        UpdateHeaderText();
+    }
+
+    private void OnTabTitleChanged(object? sender, string e)
+    {
+        UpdateHeaderText();
+    }
+
+    private void UpdateHeaderText()
+    {
+        HeaderText.Text = (DataContext as Tab)?.Title ?? string.Empty;
+    }
+}
