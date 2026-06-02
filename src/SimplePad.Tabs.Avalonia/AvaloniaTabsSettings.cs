3a2eca8583ef7cab93b4a1e70a25109444eb1e87
h82258652 <842053625@qq.com>
2026-06-02 16:20:45 +0800

    x

---
+++ src/SimplePad.Tabs.Avalonia/AvaloniaTabsSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SimplePad.Tabs;
+
+internal sealed class AvaloniaTabsSettings : ITabsSettings
+{
+    private OpenFileBehavior _openFileBehavior = OpenFileBehavior.NewTab;
+
+    public event EventHandler<OpenFileBehavior>? OpenFileBehaviorChanged;
+
+    public OpenFileBehavior OpenFileBehavior
+    {
+        get => _openFileBehavior;
+        set
+        {
+            if (_openFileBehavior != value)
+            {
+                _openFileBehavior = value;
+                OpenFileBehaviorChanged?.Invoke(this, value);
+            }
+        }
+    }
+
+    public Task LoadAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SaveAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
