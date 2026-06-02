2e11a9c498cdad247cd34cdf0860e2042255320d
h82258652 <842053625@qq.com>
2026-06-02 17:13:48 +0800

    x

---
+++ src/SimplePad.Settings.WinUI/SettingsView.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace SimplePad.Settings;
+
+public sealed partial class SettingsView : UserControl
+{
+    public SettingsView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/SimplePad.Settings.WinUI/SimplePadSettingsWinUIModule.cs
@@ -0,0 +1,8 @@
+﻿using SimplePad.Core.Modularity;
+
+namespace SimplePad.Settings;
+
+public sealed class SimplePadSettingsWinUIModule : AppModuleBase
+{
+    public override DependsOn DependModules => DependsOn.Create<SimplePadSettingsModule>();
+}
