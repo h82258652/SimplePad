bbdbe1abfcd6645ccb895c531f8c700ca8630f26
h82258652 <842053625@qq.com>
2026-06-02 17:35:21 +0800

    x

---
+++ src/SimplePad.Settings.Avalonia/BackButton.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+
+namespace SimplePad.Settings;
+
+public sealed class BackButton : Button
+{
+    private readonly SettingsState _settingsState;
+
+    public BackButton()
+    {
+        _settingsState = ServiceLocator.Current.GetRequiredService<SettingsState>();
+
+        Click += OnClick;
+    }
+
+    private void OnClick(object? sender, RoutedEventArgs e)
+    {
+        _settingsState.IsVisible = false;
+    }
+}
---
+++ src/SimplePad.Settings.Avalonia/SettingsButton.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+
+namespace SimplePad.Settings;
+
+public sealed class SettingsButton : Button
+{
+    private readonly SettingsState _settingsState;
+
+    public SettingsButton()
+    {
+        _settingsState = ServiceLocator.Current.GetRequiredService<SettingsState>();
+
+        Click += OnClick;
+    }
+
+    private void OnClick(object? sender, RoutedEventArgs e)
+    {
+        _settingsState.IsVisible = true;
+    }
+}
