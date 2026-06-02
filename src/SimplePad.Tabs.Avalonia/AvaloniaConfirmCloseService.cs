04ae33c6f50492b62bd8c7114f8ecb6f6073d1e4
h82258652 <842053625@qq.com>
2026-06-02 16:26:26 +0800

    x

---
+++ src/SimplePad.Tabs.Avalonia/AvaloniaConfirmCloseService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SimplePad.Tabs;
+
+internal class AvaloniaConfirmCloseService : IConfirmCloseService
+{
+    public Task<ConfirmCloseResult> ConfirmCloseAsync(Tab tab)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/SimplePad.Tabs.Avalonia/SimplePadTabsAvaloniaModule.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core.Modularity;
+
+namespace SimplePad.Tabs;
+
+public sealed class SimplePadTabsAvaloniaModule : AppModuleBase
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        base.ConfigureServices(context);
+
+        context.Services.AddSingleton<ITabsSettings, AvaloniaTabsSettings>();
+        context.Services.AddTransient<IConfirmCloseService, AvaloniaConfirmCloseService>();
+    }
+}
