34213b8acae65d91d4e10637c1dd10cee1380355
h82258652 <842053625@qq.com>
2026-06-02 16:08:27 +0800

    x

--- src/SimplePad.WPF.App/Program.cs
+++ src/SimplePad.WPF.App/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SimplePad.Core.Modularity;
+using SimplePad.Editor;
 using SimplePad.Fonts;
+using SimplePad.Search;
+using SimplePad.StatusBar;
+using SimplePad.Tabs;
 using SimplePad.Themes;
 
 namespace SimplePad.App;
@@ -17,8 +22,14 @@ public static class Program
             .Build();
         host.Start();
 
-        host.Services.GetRequiredService<IFontSettings>().LoadAsync().GetAwaiter().GetResult();
-        host.Services.GetRequiredService<IThemeSettings>().LoadAsync().GetAwaiter().GetResult();
+        // WPF application can't not use async Main method, so we need to load settings synchronously.
+        Task.WhenAll(
+             host.Services.GetRequiredService<IEditorSettings>().LoadAsync(),
+             host.Services.GetRequiredService<IFontSettings>().LoadAsync(),
+             host.Services.GetRequiredService<ISearchSettings>().LoadAsync(),
+             host.Services.GetRequiredService<IStatusBarSettings>().LoadAsync(),
+             host.Services.GetRequiredService<ITabsSettings>().LoadAsync(),
+             host.Services.GetRequiredService<IThemeSettings>().LoadAsync()).GetAwaiter().GetResult();
 
         App app = new(host.Services);
         app.Run();
