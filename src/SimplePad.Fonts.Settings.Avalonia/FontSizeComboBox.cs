68ff1a31051916d9209a2d7650093cf0a9601968
h82258652 <842053625@qq.com>
2026-06-02 15:03:31 +0800

    x

---
+++ src/SimplePad.Fonts.Settings.Avalonia/FontSizeComboBox.cs
@@ -0,0 +1,73 @@
+﻿using Avalonia.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+
+namespace SimplePad.Fonts;
+
+public sealed class FontSizeComboBox : ComboBox
+{
+    private static readonly int[] VisibleFontSizeList =
+    [
+        8,
+        9,
+        10,
+        11,
+        12,
+        14,
+        16,
+        18,
+        20,
+        22,
+        24,
+        26,
+        28,
+        36,
+        48,
+        72,
+    ];
+
+    private readonly IFontSettings _fontSettings;
+
+    public FontSizeComboBox()
+    {
+        _fontSettings = ServiceLocator.Current.GetRequiredService<IFontSettings>();
+
+        for (int i = AppFontSizeConstants.Minimum; i <= AppFontSizeConstants.Maximum; i++)
+        {
+            ComboBoxItem comboBoxItem = new()
+            {
+                IsVisible = VisibleFontSizeList.Contains(i),
+                Content = i,
+                IsSelected = _fontSettings.FontSize == i,
+            };
+            Items.Add(comboBoxItem);
+        }
+
+        IsEditable = true;
+
+        _fontSettings.FontSizeChanged += OnFontSettingsFontSizeChanged;
+        SelectionChanged += OnSelectionChanged;
+    }
+
+    private void OnFontSettingsFontSizeChanged(object? sender, int e)
+    {
+        UpdateSelectedItem();
+    }
+
+    private void OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        if (
+            SelectedItem is ComboBoxItem selectedItem
+            && selectedItem.Content is int fontSize
+            && fontSize >= AppFontSizeConstants.Minimum
+            && fontSize <= AppFontSizeConstants.Maximum)
+        {
+            _fontSettings.FontSize = fontSize;
+        }
+    }
+
+    private void UpdateSelectedItem()
+    {
+        SelectedItem = Items.OfType<ComboBoxItem>().FirstOrDefault(comboBoxItem => Equals(comboBoxItem.Content, _fontSettings.FontSize));
+    }
+}
