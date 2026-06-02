4edd8d8723af813291ef543fa063a1e4a8cbfcb0
h82258652 <842053625@qq.com>
2026-06-02 17:14:49 +0800

    x

---
+++ src/SimplePad.Settings.Avalonia/SettingsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SimplePad.Settings;
+
+public sealed partial class SettingsView : UserControl
+{
+    public SettingsView()
+    {
+        InitializeComponent();
+    }
+}
