422622f3fd4603e0db7619cc1b76871c61bef4b4
h82258652 <842053625@qq.com>
2026-06-02 15:56:01 +0800

    x

---
+++ src/SimplePad.StatusBar.Avalonia/SimplePadStatusBarAvaloniaModule.cs
@@ -0,0 +1,15 @@
+﻿using SimplePad.Core.Modularity;
+
+namespace SimplePad.StatusBar;
+
+public sealed class SimplePadStatusBarAvaloniaModule : AppModuleBase
+{
+    public override DependsOn DependModules => DependsOn.Create<SimplePadStatusBarModule>();
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        base.ConfigureServices(context);
+
+        context.Services.AddSingleton<IStatusBarSettings, AvaloniaStatusBarSettings>();
+    }
+}
---
+++ src/SimplePad.StatusBar.WinUI/SimplePadStatusBarWinUIModule.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core.Modularity;
+
+namespace SimplePad.StatusBar;
+
+public sealed class SimplePadStatusBarWinUIModule : AppModuleBase
+{
+    public override DependsOn DependModules => DependsOn.Create<SimplePadStatusBarModule>();
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        base.ConfigureServices(context);
+
+        context.Services.AddSingleton<IStatusBarSettings, WinUIStatusBarSettings>();
+    }
+}
