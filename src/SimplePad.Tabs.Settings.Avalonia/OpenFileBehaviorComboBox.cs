1f7f59b2cb1e5f33473fd4f05807d1fdebf30af3
h82258652 <842053625@qq.com>
2026-06-02 16:19:27 +0800

    x

--- src/SimplePad.Tabs.Settings.Avalonia/OpenFileBehaviorComboBox.cs
+++ src/SimplePad.Tabs.Settings.Avalonia/OpenFileBehaviorComboBox.cs
@@ -1,10 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Linq;
+using Avalonia.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
 
-namespace SimplePad.Tabs
+namespace SimplePad.Tabs;
+
+public sealed class OpenFileBehaviorComboBox : ComboBox
 {
-    internal class OpenFileBehaviorComboBox
+    private readonly OpenFileBehavior[] _items;
+    private readonly ITabsSettings _tabsSettings;
+
+    public OpenFileBehaviorComboBox()
     {
+        _tabsSettings = ServiceLocator.Current.GetRequiredService<ITabsSettings>();
+        _items =
+        [
+            OpenFileBehavior.NewTab,
+            OpenFileBehavior.NewWindow
+        ];
+
+        ItemsSource = _items;
+        UpdateSelectedItem();
+
+        _tabsSettings.OpenFileBehaviorChanged += OnTabsSettingsOpenFileBehaviorChanged;
+        SelectionChanged += OnSelectionChanged;
+    }
+
+    private void OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        if (SelectedItem is OpenFileBehavior selectedItem)
+        {
+            _tabsSettings.OpenFileBehavior = selectedItem;
+        }
+    }
+
+    private void OnTabsSettingsOpenFileBehaviorChanged(object? sender, OpenFileBehavior e)
+    {
+        UpdateSelectedItem();
+    }
+
+    private void UpdateSelectedItem()
+    {
+        SelectedItem = _items.FirstOrDefault(item => item.Value == _tabsSettings.OpenFileBehavior.Value);
     }
 }
