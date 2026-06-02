afa7dfde52d2ebd7cecf5b191189285fb62d4b0e
h82258652 <842053625@qq.com>
2026-06-02 17:58:05 +0800

    x

---
+++ src/SimplePad.Menu.Avalonia/GoToLineMenuItem.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SimplePad.Menu;
+
+public partial class GoToLineMenuItem : MenuItem
+{
+    public GoToLineMenuItem()
+    {
+        InitializeComponent();
+    }
+}
