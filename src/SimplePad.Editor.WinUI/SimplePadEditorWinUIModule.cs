cee2fedf582041cbd9a365c7af647e50feef59a7
h82258652 <842053625@qq.com>
2026-06-08 11:25:54 +0800

    x

--- src/SimplePad.Editor.WinUI/SimplePadEditorWinUIModule.cs
+++ src/SimplePad.Editor.WinUI/SimplePadEditorWinUIModule.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using SimplePad.Core.Modularity;
+using SimplePad.Fonts;
+using SimplePad.Themes;
 
 namespace SimplePad.Editor;
 
 public sealed class SimplePadEditorWinUIModule : AppModuleBase
 {
+    public override DependsOn DependModules => DependsOn.Create<
+        SimplePadEditorModule, 
+        SimplePadFontsWinUIModule,
+        SimplePadThemesWinUIModule>();
+
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
         base.ConfigureServices(context);
