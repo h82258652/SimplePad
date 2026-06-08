74c78c6c3c218b871f469f5e6cdace4e91304d83
h82258652 <842053625@qq.com>
2026-06-08 23:26:30 +0800

    x

--- src/SimplePad.Search.WinUI/SearchControl.xaml.cs
+++ src/SimplePad.Search.WinUI/SearchControl.xaml.cs
@@ -1,11 +1,46 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using SimplePad.Core;
+using System;
 
 namespace SimplePad.Search;
 
 public sealed partial class SearchControl : UserControl
 {
+    private readonly WinUISearchNotificationService _searchNotificationService;
+    private readonly SearchViewState _searchViewState;
+
     public SearchControl()
     {
+        _searchViewState = ServiceLocator.Current.GetRequiredService<SearchViewState>();
+
         InitializeComponent();
+        _searchNotificationService.Configure(ShowNotificationFlyout, HideNotificationFlyout, SetNotificationText);
+
+        Visibility = _searchViewState.IsVisible ? Visibility.Visible : Visibility.Collapsed;
+
+        _searchViewState.IsVisibleChanged += OnSearchViewStateIsVisibleChanged;
+    }
+
+    private void HideNotificationFlyout()
+    {
+        NotificationFlyout.Hide();
+    }
+
+    private void OnSearchViewStateIsVisibleChanged(object? sender, bool e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void SetNotificationText(string notificationText)
+    {
+        NotificationText.Text = notificationText;
+    }
+
+    private void ShowNotificationFlyout()
+    {
+        FlyoutBase.ShowAttachedFlyout(RootContainer);
     }
 }
