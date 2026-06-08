29fbda413adfc4abaae30879fcfd497e34f3cbff
h82258652 <842053625@qq.com>
2026-06-08 22:43:00 +0800

    x

---
+++ src/SimplePad.Search.Avalonia/SimplePadSearchAvaloniaModule.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core.Modularity;
+
+namespace SimplePad.Search;
+
+public sealed class SimplePadSearchAvaloniaModule : AppModuleBase
+{
+    public override DependsOn DependModules => DependsOn.Create<SimplePadSearchModule>();
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        base.ConfigureServices(context);
+
+        context.Services.AddSingleton<ISearchSettings, AvaloniaSearchSettings>();
+    }
+}
