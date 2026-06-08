39622348501edb00b1c128734fdef6c4fdd21586
h82258652 <842053625@qq.com>
2026-06-08 11:19:01 +0800

    x

--- src/SimplePad.Editor.Settings.WinUI/IsWordWrapSettingsControl.xaml.cs
+++ src/SimplePad.Editor.Settings.WinUI/IsWordWrapSettingsControl.xaml.cs
@@ -1,28 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using SimplePad.Core;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace SimplePad.Editor;
 
-namespace SimplePad.Editor
+public sealed partial class IsWordWrapSettingsControl : UserControl
 {
-    public sealed partial class IsWordWrapSettingsControl : UserControl
+    private readonly IEditorSettings _editorSettings;
+
+    public IsWordWrapSettingsControl()
     {
-        public IsWordWrapSettingsControl()
-        {
-            InitializeComponent();
-        }
+        _editorSettings = ServiceLocator.Current.GetRequiredService<IEditorSettings>();
+
+        InitializeComponent();
+
+        UpdateIsWordWrapToggleSwitch();
+
+        _editorSettings.IsWordWrapChanged += OnEditorSettingsIsWordWrapChanged;
+    }
+
+    private void OnEditorSettingsIsWordWrapChanged(object sender, bool e)
+    {
+        UpdateIsWordWrapToggleSwitch();
+    }
+
+    private void OnIsWordWrapToggleSwitchToggled(object sender, RoutedEventArgs e)
+    {
+        _editorSettings.IsWordWrap = IsWordWrapToggleSwitch.IsOn;
+    }
+
+    private void UpdateIsWordWrapToggleSwitch()
+    {
+        IsWordWrapToggleSwitch.IsOn = _editorSettings.IsWordWrap;
     }
 }
