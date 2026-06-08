d73465dc31c4eca324a870040834b663c07db99b
h82258652 <842053625@qq.com>
2026-06-08 23:01:10 +0800

    x

--- src/SimplePad.Menu.Avalonia/CopyMenuItem.axaml.cs
+++ src/SimplePad.Menu.Avalonia/CopyMenuItem.axaml.cs
@@ -1,13 +1,21 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using SimplePad.Editor;
 
 namespace SimplePad.Menu;
 
 public partial class CopyMenuItem : MenuItem
 {
+    public static readonly StyledProperty<IAppTextBox?> TextBoxProperty = AvaloniaProperty.Register<CopyMenuItem, IAppTextBox?>(nameof(TextBox));
+
     public CopyMenuItem()
     {
         InitializeComponent();
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
     }
 }
