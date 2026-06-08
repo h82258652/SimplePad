e39d9d49a20a25d71f4177b1060595366cbe09cd
h82258652 <842053625@qq.com>
2026-06-08 22:37:26 +0800

    x

--- src/SimplePad.Search.WinUI/WinUISearchSettings.cs
+++ src/SimplePad.Search.WinUI/WinUISearchSettings.cs
@@ -1,26 +1,73 @@
-﻿using SimplePad.Core.Settings;
+﻿using Microsoft.Windows.Storage;
+using SimplePad.Core.Settings;
 using System;
 using System.Threading.Tasks;
+using Windows.Foundation.Collections;
 
 namespace SimplePad.Search;
 
 internal sealed class WinUISearchSettings : AppSettingsBase, ISearchSettings
 {
+    private bool _isMatchCase;
+    private bool _isWrapAround = true;
+
     public event EventHandler<bool>? IsMatchCaseChanged;
 
     public event EventHandler<bool>? IsWrapAroundChanged;
 
-    public bool IsMatchCase { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-   
-    public bool IsWrapAround { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public bool IsMatchCase
+    {
+        get => _isMatchCase;
+        set
+        {
+            if (_isMatchCase != value)
+            {
+                _isMatchCase = value;
+                IsMatchCaseChanged?.Invoke(this, value);
+            }
+        }
+    }
+
+    public bool IsWrapAround
+    {
+        get => _isWrapAround;
+        set
+        {
+            if (_isWrapAround != value)
+            {
+                _isWrapAround = value;
+                IsWrapAroundChanged?.Invoke(this, value);
+            }
+        }
+    }
 
     public override Task LoadAsync()
     {
-        throw new NotImplementedException();
+        IPropertySet settingsValue = GetSettings();
+        if (settingsValue.TryGetValue(nameof(IsMatchCase), out object? isMatchCase))
+        {
+            IsMatchCase = (bool)isMatchCase;
+        }
+
+        if (settingsValue.TryGetValue(nameof(IsWrapAround), out object? isWrapAround))
+        {
+            IsWrapAround = (bool)isWrapAround;
+        }
+
+        return Task.CompletedTask;
     }
 
     public override Task SaveAsync()
     {
-        throw new NotImplementedException();
+        IPropertySet settingsValue = GetSettings();
+        settingsValue[nameof(IsMatchCase)] = IsMatchCase;
+        settingsValue[nameof(IsWrapAround)] = IsWrapAround;
+
+        return Task.CompletedTask;
+    }
+
+    private static IPropertySet GetSettings()
+    {
+        return ApplicationData.GetDefault().LocalSettings.CreateContainer("Search", ApplicationDataCreateDisposition.Always).Values;
     }
 }
