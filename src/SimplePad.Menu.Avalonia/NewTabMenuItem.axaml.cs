c523d99ab38c64aedc1ba06e426cb0ebcb5c9a9f
h82258652 <842053625@qq.com>
2026-06-08 22:54:23 +0800

    x

---
+++ src/SimplePad.Menu.Avalonia/NewTabMenuItem.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SimplePad.Menu;
+
+public partial class NewTabMenuItem : UserControl
+{
+    public NewTabMenuItem()
+    {
+        InitializeComponent();
+    }
+}
