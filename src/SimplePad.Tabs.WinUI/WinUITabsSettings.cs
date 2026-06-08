e211b06f4a1fd54f29229057342fe6d4a5bb9590
h82258652 <842053625@qq.com>
2026-06-08 22:02:54 +0800

    x

---
+++ src/SimplePad.Tabs.WinUI/WinUITabsSettings.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Windows.Storage;
+using SimplePad.Core.Settings;
+using System;
+using System.Threading.Tasks;
+using Windows.Foundation.Collections;
+
+namespace SimplePad.Tabs;
+
+internal sealed class WinUITabsSettings : AppSettingsBase, ITabsSettings
+{
+    private OpenFileBehavior _openFileBehavior = OpenFileBehavior.NewTab;
+
+    public event EventHandler<OpenFileBehavior>? OpenFileBehaviorChanged;
+
+    public OpenFileBehavior OpenFileBehavior
+    {
+        get => _openFileBehavior;
+        set
+        {
+            if (_openFileBehavior != value)
+            {
+                _openFileBehavior = value;
+                OpenFileBehaviorChanged?.Invoke(this, value);
+            }
+        }
+    }
+
+    public override Task LoadAsync()
+    {
+        IPropertySet settingsValue = GetSettings();
+        if (settingsValue.TryGetValue(nameof(OpenFileBehavior), out object? openFileBehavior))
+        {
+            OpenFileBehavior = OpenFileBehavior.FromValue((int)openFileBehavior);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public override Task SaveAsync()
+    {
+        IPropertySet settingsValue = GetSettings();
+        settingsValue[nameof(OpenFileBehavior)] = OpenFileBehavior.Value;
+
+        return Task.CompletedTask;
+    }
+
+    private static IPropertySet GetSettings()
+    {
+        return ApplicationData.GetDefault().LocalSettings.CreateContainer("Tabs", ApplicationDataCreateDisposition.Always).Values;
+    }
+}
