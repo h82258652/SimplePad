68db892b9f9885c8ab6de1d1c3065e92c3da9d02
h82258652 <842053625@qq.com>
2026-06-08 23:28:27 +0800

    x

--- src/SimplePad.Search.Avalonia/SearchControl.axaml.cs
+++ src/SimplePad.Search.Avalonia/SearchControl.axaml.cs
@@ -1,11 +1,17 @@
 using Avalonia.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
 
 namespace SimplePad.Search;
 
 public partial class SearchControl : UserControl
 {
+    private readonly SearchViewState _searchViewState;
+
     public SearchControl()
     {
+        _searchViewState = ServiceLocator.Current.GetRequiredService<SearchViewState>();
+
         InitializeComponent();
     }
 }
