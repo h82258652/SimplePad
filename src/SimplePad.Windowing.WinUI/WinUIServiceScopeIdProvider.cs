58dd7fbeda9f90597e6e5715d9ac1e3687862adf
h82258652 <842053625@qq.com>
2026-06-08 22:50:55 +0800

    x

---
+++ src/SimplePad.Windowing.WinUI/WinUIServiceScopeIdProvider.cs
@@ -0,0 +1,24 @@
+﻿using SimplePad.Core;
+using System;
+
+namespace SimplePad.Windowing;
+
+internal sealed class WinUIServiceScopeIdProvider : IServiceScopeIdProvider
+{
+    private readonly IAppWindowManager _appWindowManager;
+
+    public WinUIServiceScopeIdProvider(IAppWindowManager appWindowManager)
+    {
+        _appWindowManager = appWindowManager;
+    }
+
+    public int? Get()
+    {
+        if (_appWindowManager.CurrentWindow is { } currentWindow)
+        {
+            return currentWindow.GetHashCode();
+        }
+
+        return null;
+    }
+}
