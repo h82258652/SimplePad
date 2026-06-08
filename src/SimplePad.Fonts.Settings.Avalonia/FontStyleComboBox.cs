cd0e1780f1090ec4c22ae4a84c5815909a705b75
h82258652 <842053625@qq.com>
2026-06-08 11:08:10 +0800

    x

--- src/SimplePad.Fonts.Settings.Avalonia/FontStyleComboBox.cs
+++ src/SimplePad.Fonts.Settings.Avalonia/FontStyleComboBox.cs
@@ -1,10 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Linq;
+using Avalonia.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
 
-namespace SimplePad.Fonts
+namespace SimplePad.Fonts;
+
+public sealed class FontStyleComboBox : ComboBox
 {
-    internal class FontStyleComboBox
+    private readonly IFontSettings _fontSettings;
+    private readonly AppFontStyle[] _items;
+
+    public FontStyleComboBox()
     {
+        _fontSettings = ServiceLocator.Current.GetRequiredService<IFontSettings>();
+        _items =
+        [
+            AppFontStyle.Regular,
+            AppFontStyle.Italic,
+            AppFontStyle.Bold,
+            AppFontStyle.BoldItalic
+        ];
+
+        ItemsSource = _items;
+        UpdateSelectedItem();
+
+        _fontSettings.FontStyleChanged += OnFontSettingsFontStyleChanged;
+        SelectionChanged += OnSelectionChanged;
+    }
+
+    private void OnFontSettingsFontStyleChanged(object? sender, AppFontStyle e)
+    {
+        UpdateSelectedItem();
+    }
+
+    private void OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        if (SelectedItem is AppFontStyle selectedItem)
+        {
+            _fontSettings.FontStyle = selectedItem;
+        }
+    }
+
+    private void UpdateSelectedItem()
+    {
+        SelectedItem = _items.FirstOrDefault(item => item.Value == _fontSettings.FontStyle.Value);
     }
 }
