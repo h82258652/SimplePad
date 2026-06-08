901f9af10da88f513d6eb641c658234ce8ad6f4e
h82258652 <842053625@qq.com>
2026-06-08 10:53:35 +0800

    x

--- src/SimplePad.Fonts.Settings.WinUI/FontFamilyComboBox.cs
+++ src/SimplePad.Fonts.Settings.WinUI/FontFamilyComboBox.cs
@@ -1,29 +1,24 @@
 ﻿using System;
-using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Graphics.Canvas.Text;
+using Microsoft.UI.Xaml.Controls;
 using SimplePad.Core;
-using SimplePad.Core.Extensions;
-using Windows.UI.Core;
-using Windows.UI.Xaml.Controls;
 
 namespace SimplePad.Fonts;
 
 public sealed partial class FontFamilyComboBox : ComboBox
 {
-    private readonly CoreDispatcher _dispatcher;
     private readonly IFontSettings _fontSettings;
     private readonly string[] _systemFontFamilies;
 
     public FontFamilyComboBox()
     {
-        _dispatcher = Dispatcher;
         _fontSettings = ServiceLocator.Current.GetRequiredService<IFontSettings>();
         _systemFontFamilies = CanvasTextFormat.GetSystemFontFamilies();
         Array.Sort(_systemFontFamilies);
 
         DefaultStyleKey = typeof(FontFamilyComboBox);
-        DefaultStyleResourceUri = new Uri("ms-appx:///SimplePad.Fonts.Settings.UWP/FontFamilyComboBox.xaml");
+        DefaultStyleResourceUri = new Uri("ms-appx:///SimplePad.Fonts.Settings.WinUI/FontFamilyComboBox.xaml");
 
         ItemsSource = _systemFontFamilies;
         UpdateSelectedItem();
@@ -34,17 +29,15 @@ public sealed partial class FontFamilyComboBox : ComboBox
         SelectionChanged += OnSelectionChanged;
     }
 
-    private async void OnFontSettingsFontFamilyChanged(object? sender, string e)
+    private void OnFontSettingsFontFamilyChanged(object? sender, string e)
     {
-        await _dispatcher.SafeRunAsync(UpdateSelectedItem);
+        UpdateSelectedItem();
     }
 
     private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-        if (
-            SelectedItem is string selectedFontFamily
-            && _systemFontFamilies.Contains(selectedFontFamily)
-        )
+        if (SelectedItem is string selectedFontFamily
+            && _systemFontFamilies.Contains(selectedFontFamily))
         {
             _fontSettings.FontFamily = selectedFontFamily;
         }
