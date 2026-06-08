11d19febf429ec2cfad4cf7f2fd023ea01e1a135
h82258652 <842053625@qq.com>
2026-06-08 22:40:17 +0800

    x

---
+++ src/SimplePad.Search.Avalonia/AvaloniaSearchSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePad.Search;
+
+internal sealed class AvaloniaSearchSettings : ISearchSettings
+{
+    public bool IsMatchCase { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public bool IsWrapAround { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public event EventHandler<bool>? IsMatchCaseChanged;
+    public event EventHandler<bool>? IsWrapAroundChanged;
+
+    public Task LoadAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SaveAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
