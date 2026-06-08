7f07d1ab5f63a7c3b3fadefff94acc81268cc403
h82258652 <842053625@qq.com>
2026-06-08 22:39:29 +0800

    x

---
+++ src/SimplePad.Search.Avalonia/SearchControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SimplePad.Search;
+
+public partial class SearchControl : UserControl
+{
+    public SearchControl()
+    {
+        InitializeComponent();
+    }
+}
