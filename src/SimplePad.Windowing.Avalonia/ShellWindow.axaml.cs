a810f5009f48a1da3e2a544672e7cfc009346a1b
h82258652 <842053625@qq.com>
2026-06-08 23:30:06 +0800

    x

---
+++ src/SimplePad.Windowing.Avalonia/ShellWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SimplePad.Windowing;
+
+public partial class ShellWindow : Window
+{
+    public ShellWindow()
+    {
+        InitializeComponent();
+    }
+}
