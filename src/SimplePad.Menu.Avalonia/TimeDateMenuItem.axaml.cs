031c1fdfa90dd9a67ac09e84601468086a10fb2d
h82258652 <842053625@qq.com>
2026-06-08 22:20:43 +0800

    x

---
+++ src/SimplePad.Menu.Avalonia/TimeDateMenuItem.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SimplePad.Menu;
+
+public partial class TimeDateMenuItem : UserControl
+{
+    public TimeDateMenuItem()
+    {
+        InitializeComponent();
+    }
+}
