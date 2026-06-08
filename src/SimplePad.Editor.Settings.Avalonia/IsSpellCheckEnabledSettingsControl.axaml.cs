8d6c3b9329cc2cc64c4fd80dab220a9a10fe5999
h82258652 <842053625@qq.com>
2026-06-08 11:23:15 +0800

    x

--- src/SimplePad.Editor.Settings.Avalonia/IsSpellCheckEnabledSettingsControl.axaml.cs
+++ src/SimplePad.Editor.Settings.Avalonia/IsSpellCheckEnabledSettingsControl.axaml.cs
@@ -1,13 +1,38 @@
-using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
 
 namespace SimplePad.Editor;
 
 public partial class IsSpellCheckEnabledSettingsControl : UserControl
 {
+    private readonly IEditorSettings _editorSettings;
+
     public IsSpellCheckEnabledSettingsControl()
     {
+        _editorSettings = ServiceLocator.Current.GetRequiredService<IEditorSettings>();
+
         InitializeComponent();
+
+        UpdateIsSpellCheckEnabledToggleSwitch();
+
+        _editorSettings.IsSpellCheckEnabledChanged += OnEditorSettingsIsSpellCheckEnabledChanged;
+    }
+
+    private void OnEditorSettingsIsSpellCheckEnabledChanged(object? sender, bool e)
+    {
+        UpdateIsSpellCheckEnabledToggleSwitch();
+    }
+
+    private void OnIsSpellCheckEnabledToggleSwitchIsCheckedChanged(object? sender, RoutedEventArgs e)
+    {
+        _editorSettings.IsSpellCheckEnabled = IsSpellCheckEnabledToggleSwitch.IsChecked is true;
+    }
+
+    private void UpdateIsSpellCheckEnabledToggleSwitch()
+    {
+        IsSpellCheckEnabledToggleSwitch.IsChecked = _editorSettings.IsSpellCheckEnabled;
     }
 }
--- src/SimplePad.Editor.Settings.Avalonia/IsWordWrapSettingsControl.axaml.cs
+++ src/SimplePad.Editor.Settings.Avalonia/IsWordWrapSettingsControl.axaml.cs
@@ -1,16 +1,37 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
 
 namespace SimplePad.Editor;
 
 public sealed partial class IsWordWrapSettingsControl : UserControl
 {
+    private readonly IEditorSettings _editorSettings;
+
     public IsWordWrapSettingsControl()
     {
+        _editorSettings = ServiceLocator.Current.GetRequiredService<IEditorSettings>();
+
         InitializeComponent();
+
+        UpdateIsWordWrapToggleSwitch();
+
+        _editorSettings.IsWordWrapChanged += OnEditorSettingsIsWordWrapChanged;
+    }
+
+    private void OnEditorSettingsIsWordWrapChanged(object? sender, bool e)
+    {
+        UpdateIsWordWrapToggleSwitch();
     }
 
     private void OnIsWordWrapToggleSwitchIsCheckedChanged(object? sender, RoutedEventArgs e)
     {
+        _editorSettings.IsWordWrap = IsWordWrapToggleSwitch.IsChecked is true;
+    }
+
+    private void UpdateIsWordWrapToggleSwitch()
+    {
+        IsWordWrapToggleSwitch.IsChecked = _editorSettings.IsWordWrap;
     }
 }
