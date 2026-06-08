fd4a4ee90c274344f15c2e1cc83fdbee65958480
h82258652 <842053625@qq.com>
2026-06-08 11:15:47 +0800

    x

---
+++ src/SimplePad.Editor.Settings.Avalonia/IsSpellCheckEnabledSettingsControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SimplePad.Editor;
+
+public partial class IsSpellCheckEnabledSettingsControl : UserControl
+{
+    public IsSpellCheckEnabledSettingsControl()
+    {
+        InitializeComponent();
+    }
+}
