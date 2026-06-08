3422c0a49baab56d6d3b3fb0dfdb74f79b7d82df
h82258652 <842053625@qq.com>
2026-06-08 23:47:32 +0800

    x

--- src/SimplePad.Fonts.Settings.Avalonia/FontFamilyComboBox.cs
+++ src/SimplePad.Fonts.Settings.Avalonia/FontFamilyComboBox.cs
@@ -1,10 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Avalonia.Controls;
+using Avalonia.Media;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using System;
+using System.Linq;
 
-namespace SimplePad.Fonts
+namespace SimplePad.Fonts;
+
+public sealed class FontFamilyComboBox : ComboBox
 {
-    internal class FontFamilyComboBox
+    private readonly IFontSettings _fontSettings;
+    private readonly string[] _systemFontFamilies;
+
+    public FontFamilyComboBox()
     {
+        _fontSettings = ServiceLocator.Current.GetRequiredService<IFontSettings>();
+        _systemFontFamilies = [.. FontManager.Current.SystemFonts.Select(font => font.Name)];
+        Array.Sort(_systemFontFamilies);
+
+        ItemsSource = _systemFontFamilies;
+        UpdateSelectedItem();
+
+        IsEditable = true;
+
+        _fontSettings.FontFamilyChanged += OnFontSettingsFontFamilyChanged;
+        SelectionChanged += OnSelectionChanged;
+    }
+
+    private void OnFontSettingsFontFamilyChanged(object? sender, string e)
+    {
+        UpdateSelectedItem();
+    }
+
+    private void OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        if (
+            SelectedItem is string selectedFontFamily
+            && _systemFontFamilies.Contains(selectedFontFamily)
+        )
+        {
+            _fontSettings.FontFamily = selectedFontFamily;
+        }
+        else
+        {
+            UpdateSelectedItem();
+        }
+    }
+
+    private void UpdateSelectedItem()
+    {
+        SelectedItem = _fontSettings.FontFamily;
     }
 }
