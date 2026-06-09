3a97cb22447f7867cf5d93a6e8254dd5e1c40842
h82258652 <842053625@qq.com>
2026-06-09 15:53:38 +0800

    x

--- src/SimplePad.Menu.WinUI/NewWindowMenuItem.xaml.cs
+++ src/SimplePad.Menu.WinUI/NewWindowMenuItem.xaml.cs
@@ -1,11 +1,24 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml.Controls;
+using SimplePad.Core;
+using SimplePad.Windowing;
 
 namespace SimplePad.Menu;
 
 public sealed partial class NewWindowMenuItem : MenuFlyoutItem
 {
+    private readonly IAppWindowManager _appWindowManager;
+
     public NewWindowMenuItem()
     {
+        _appWindowManager = ServiceLocator.Current.GetRequiredService<IAppWindowManager>();
+
         InitializeComponent();
+    }
+
+    private async void OnClick(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        IAppWindow newAppWindow = await _appWindowManager.ShowNewWindowAsync();
+        newAppWindow.Execute(appWindow => appWindow.TabRoot.AddBlankTab());
     }
 }
