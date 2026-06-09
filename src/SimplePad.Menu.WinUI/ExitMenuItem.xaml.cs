d70d4b08c8f683258e01e504f1289ffb297014bb
h82258652 <842053625@qq.com>
2026-06-09 15:51:03 +0800

    x

---
+++ src/SimplePad.Menu.WinUI/ExitMenuItem.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace SimplePad.Menu;
+
+public sealed partial class ExitMenuItem : MenuFlyoutItem
+{
+    public ExitMenuItem()
+    {
+        InitializeComponent();
+    }
+}
