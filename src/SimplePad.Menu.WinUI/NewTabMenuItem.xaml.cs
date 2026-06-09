8dbd4684e4c73d822f7c66992e57093f070457cf
h82258652 <842053625@qq.com>
2026-06-09 15:47:51 +0800

    x

--- src/SimplePad.Menu.WinUI/NewTabMenuItem.xaml.cs
+++ src/SimplePad.Menu.WinUI/NewTabMenuItem.xaml.cs
@@ -1,11 +1,25 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using SimplePad.Tabs;
 
 namespace SimplePad.Menu;
 
 public sealed partial class NewTabMenuItem : MenuFlyoutItem
 {
+    public static readonly DependencyProperty TabRootProperty = DependencyProperty.Register(
+        nameof(TabRoot),
+        typeof(TabRoot),
+        typeof(NewTabMenuItem),
+        null);
+
     public NewTabMenuItem()
     {
         InitializeComponent();
+    }
+
+    public TabRoot? TabRoot
+    {
+        get => (TabRoot?)GetValue(TabRootProperty);
+        set => SetValue(TabRootProperty, value);
     }
 }
