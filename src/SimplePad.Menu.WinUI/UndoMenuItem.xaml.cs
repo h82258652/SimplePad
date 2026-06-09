68f93d29e1570b7cebeebe8427d66e2a7462a12b
h82258652 <842053625@qq.com>
2026-06-09 15:32:25 +0800

    x

--- src/SimplePad.Menu.WinUI/UndoMenuItem.xaml.cs
+++ src/SimplePad.Menu.WinUI/UndoMenuItem.xaml.cs
@@ -1,28 +1,11 @@
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace SimplePad.Menu;
 
-namespace SimplePad.Menu
+public sealed partial class UndoMenuItem : MenuFlyoutItem
 {
-    public sealed partial class UndoMenuItem : UserControl
+    public UndoMenuItem()
     {
-        public UndoMenuItem()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
