0979b331776044ef6c3087a8eb7433586df2233d
h82258652 <842053625@qq.com>
2026-06-09 15:42:24 +0800

    x

--- src/SimplePad.Menu.WinUI/CutMenuItem.xaml.cs
+++ src/SimplePad.Menu.WinUI/CutMenuItem.xaml.cs
@@ -1,11 +1,25 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using SimplePad.Editor;
 
 namespace SimplePad.Menu;
 
 public sealed partial class CutMenuItem : MenuFlyoutItem
 {
+    public static readonly DependencyProperty TextBoxProperty = DependencyProperty.Register(
+        nameof(TextBox),
+        typeof(IAppTextBox),
+        typeof(CutMenuItem),
+        null);
+
     public CutMenuItem()
     {
         InitializeComponent();
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => (IAppTextBox?)GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
     }
 }
