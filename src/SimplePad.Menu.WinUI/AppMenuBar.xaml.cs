70cb65de73d461a7f9288e90ecbfbbabb1b8a85f
h82258652 <842053625@qq.com>
2026-06-09 15:27:47 +0800

    x

---
+++ src/SimplePad.Menu.WinUI/AppMenuBar.xaml.cs
@@ -0,0 +1,38 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using SimplePad.Editor;
+using SimplePad.Tabs;
+
+namespace SimplePad.Menu;
+
+public sealed partial class AppMenuBar : MenuBar
+{
+    public static readonly DependencyProperty TabProperty = DependencyProperty.Register(
+        nameof(Tab),
+        typeof(Tab),
+        typeof(AppMenuBar),
+        null);
+
+    public static readonly DependencyProperty TextBoxProperty = DependencyProperty.Register(
+        nameof(TextBox),
+        typeof(IAppTextBox),
+        typeof(AppMenuBar),
+        null);
+
+    public AppMenuBar()
+    {
+        InitializeComponent();
+    }
+
+    public Tab? Tab
+    {
+        get => (Tab?)GetValue(TabProperty);
+        set => SetValue(TabProperty, value);
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => (IAppTextBox?)GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
+    }
+}
