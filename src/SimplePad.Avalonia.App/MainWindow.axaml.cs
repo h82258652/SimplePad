c989a0a70fa59f6a5403b7b180177ce2198e289a
h82258652 <842053625@qq.com>
2026-06-09 08:56:21 +0800

    x

---
+++ src/SimplePad.Avalonia.App/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SimplePad.Avalonia.App
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
