345bbe0c62696c9858d8d9ad1da2162de4fb970e
h82258652 <842053625@qq.com>
2026-06-09 15:50:08 +0800

    x

--- src/SimplePad.Menu.WinUI/PasteMenuItem.xaml.cs
+++ src/SimplePad.Menu.WinUI/PasteMenuItem.xaml.cs
@@ -1,11 +1,25 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using SimplePad.Editor;
 
 namespace SimplePad.Menu;
 
 public sealed partial class PasteMenuItem : MenuFlyoutItem
 {
+    public static readonly DependencyProperty TextBoxProperty = DependencyProperty.Register(
+        nameof(TextBox),
+        typeof(IAppTextBox),
+        typeof(PasteMenuItem),
+        null);
+
     public PasteMenuItem()
     {
         InitializeComponent();
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => (IAppTextBox?)GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
     }
 }
