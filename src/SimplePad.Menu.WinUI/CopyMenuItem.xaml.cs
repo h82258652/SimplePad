9fb66bf1f873c066d8c333852f5a9fbd9681103d
h82258652 <842053625@qq.com>
2026-06-09 15:41:08 +0800

    x

--- src/SimplePad.Menu.WinUI/CopyMenuItem.xaml.cs
+++ src/SimplePad.Menu.WinUI/CopyMenuItem.xaml.cs
@@ -1,28 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace SimplePad.Menu;
 
-namespace SimplePad.Menu
+public sealed partial class CopyMenuItem : MenuFlyoutItem
 {
-    public sealed partial class CopyMenuItem : UserControl
+    public CopyMenuItem()
     {
-        public CopyMenuItem()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
