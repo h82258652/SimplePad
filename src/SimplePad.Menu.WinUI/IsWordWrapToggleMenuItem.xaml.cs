59c18f1a1fd0ecf9d8f6c478012a2d2d9f848d55
h82258652 <842053625@qq.com>
2026-06-09 16:05:11 +0800

    x

--- src/SimplePad.Menu.WinUI/IsWordWrapToggleMenuItem.xaml.cs
+++ src/SimplePad.Menu.WinUI/IsWordWrapToggleMenuItem.xaml.cs
@@ -1,28 +1,51 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using SimplePad.Core;
+using SimplePad.Editor;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace SimplePad.Menu;
 
-namespace SimplePad.Menu
+public sealed partial class IsWordWrapToggleMenuItem : ToggleMenuFlyoutItem
 {
-    public sealed partial class IsWordWrapToggleMenuItem : UserControl
+    public static readonly DependencyProperty TextBoxProperty = DependencyProperty.Register(
+        nameof(TextBox),
+        typeof(IAppTextBox),
+        typeof(IsWordWrapToggleMenuItem),
+        null);
+
+    private readonly IEditorSettings _editorSettings;
+
+    public IsWordWrapToggleMenuItem()
     {
-        public IsWordWrapToggleMenuItem()
-        {
-            InitializeComponent();
-        }
+        _editorSettings = ServiceLocator.Current.GetRequiredService<IEditorSettings>();
+
+        InitializeComponent();
+
+        UpdateIsChecked();
+
+        _editorSettings.IsWordWrapChanged += OnEditorSettingsIsWordWrapChanged;
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => (IAppTextBox?)GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
+    }
+
+    private void OnClick(object sender, RoutedEventArgs e)
+    {
+        _editorSettings.IsWordWrap = IsChecked;
+        TextBox?.Focus();
+    }
+
+    private void OnEditorSettingsIsWordWrapChanged(object? sender, bool e)
+    {
+        UpdateIsChecked();
+    }
+
+    private void UpdateIsChecked()
+    {
+        IsChecked = _editorSettings.IsWordWrap;
     }
 }
