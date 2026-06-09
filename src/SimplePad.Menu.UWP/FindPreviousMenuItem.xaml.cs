a5ca3d17ded3743d9b6ca53c4abc58c07481d81d
h82258652 <842053625@qq.com>
2026-06-09 15:16:36 +0800

    x

---
+++ src/SimplePad.Menu.UWP/FindPreviousMenuItem.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace SimplePad.Menu;
+
+public sealed partial class FindPreviousMenuItem : MenuFlyoutItem
+{
+    public FindPreviousMenuItem()
+    {
+        InitializeComponent();
+    }
+}
