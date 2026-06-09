4bcf788de3f218c8e4f0521c00d4bab52c58e195
h82258652 <842053625@qq.com>
2026-06-09 15:35:50 +0800

    x

--- src/SimplePad.Menu.WinUI/ZoomOutMenuItem.xaml.cs
+++ src/SimplePad.Menu.WinUI/ZoomOutMenuItem.xaml.cs
@@ -1,11 +1,53 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using SimplePad.Core;
+using SimplePad.Editor;
 
 namespace SimplePad.Menu;
 
 public sealed partial class ZoomOutMenuItem : MenuFlyoutItem
 {
+    public static readonly DependencyProperty TextBoxProperty = DependencyProperty.Register(
+        nameof(TextBox),
+        typeof(IAppTextBox),
+        typeof(ZoomOutMenuItem),
+        null);
+
+    private readonly EditorZoomState _editorZoomState;
+
     public ZoomOutMenuItem()
     {
+        _editorZoomState = ServiceLocator.Current.GetRequiredService<EditorZoomState>();
+
         InitializeComponent();
+
+        UpdateIsEnabled();
+
+        _editorZoomState.CanZoomOutChanged += OnEditorZoomStateCanZoomOutChanged;
+    }
+
+    private async void OnClick(object sender, RoutedEventArgs e)
+    {
+        _editorZoomState.ZoomOut();
+        await Task.Yield();
+        TextBox?.Focus();
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => (IAppTextBox?)GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
+    }
+
+    private void OnEditorZoomStateCanZoomOutChanged(object? sender, bool e)
+    {
+        UpdateIsEnabled();
+    }
+
+    private void UpdateIsEnabled()
+    {
+        IsEnabled = _editorZoomState.CanZoomOut;
     }
 }
