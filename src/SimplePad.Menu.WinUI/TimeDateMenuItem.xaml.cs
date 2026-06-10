b591deec5285d1d066facda24a593f1d830a17ea
h82258652 <842053625@qq.com>
2026-06-10 16:58:27 +0800

    x

--- src/SimplePad.Menu.WinUI/TimeDateMenuItem.xaml.cs
+++ src/SimplePad.Menu.WinUI/TimeDateMenuItem.xaml.cs
@@ -1,28 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using SimplePad.Editor;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace SimplePad.Menu;
 
-namespace SimplePad.Menu
+public sealed partial class TimeDateMenuItem : MenuFlyoutItem
 {
-    public sealed partial class TimeDateMenuItem : UserControl
+    public static readonly DependencyProperty TextBoxProperty = DependencyProperty.Register(
+        nameof(TextBox),
+        typeof(IAppTextBox),
+        typeof(TimeDateMenuItem),
+        null);
+
+    public TimeDateMenuItem()
     {
-        public TimeDateMenuItem()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => (IAppTextBox?)GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
     }
 }
