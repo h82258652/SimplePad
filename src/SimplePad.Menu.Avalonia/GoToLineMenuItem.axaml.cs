ed38d4651bb94b584b29441ddf86f1e7b0c9d41a
h82258652 <842053625@qq.com>
2026-06-10 17:05:20 +0800

    x

--- src/SimplePad.Menu.Avalonia/GoToLineMenuItem.axaml.cs
+++ src/SimplePad.Menu.Avalonia/GoToLineMenuItem.axaml.cs
@@ -1,13 +1,21 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using SimplePad.Editor;
 
 namespace SimplePad.Menu;
 
 public partial class GoToLineMenuItem : MenuItem
 {
+    public static readonly StyledProperty<IAppTextBox?> TextBoxProperty = AvaloniaProperty.Register<GoToLineMenuItem, IAppTextBox?>(nameof(TextBox));
+
     public GoToLineMenuItem()
     {
         InitializeComponent();
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
     }
 }
---
+++ src/SimplePad.Menu.WinUI/GoToLineMenuItem.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace SimplePad.Menu;
+
+public sealed partial class GoToLineMenuItem : MenuFlyoutItem
+{
+    public GoToLineMenuItem()
+    {
+        InitializeComponent();
+    }
+}
