03c7b63915f960aed84d28b8bd8e4d895f6e8227
h82258652 <842053625@qq.com>
2026-06-10 17:06:48 +0800

    x

--- src/SimplePad.Menu.WinUI/GoToLineMenuItem.xaml.cs
+++ src/SimplePad.Menu.WinUI/GoToLineMenuItem.xaml.cs
@@ -1,11 +1,25 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using SimplePad.Editor;
 
 namespace SimplePad.Menu;
 
 public sealed partial class GoToLineMenuItem : MenuFlyoutItem
 {
+    public static readonly DependencyProperty TextBoxProperty = DependencyProperty.Register(
+        nameof(TextBox),
+        typeof(IAppTextBox),
+        typeof(GoToLineMenuItem),
+        null);
+
     public GoToLineMenuItem()
     {
         InitializeComponent();
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => (IAppTextBox?)GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
     }
 }
