e85341b92381c1b2f4e561a817d9e3d5aa3784d1
h82258652 <842053625@qq.com>
2026-06-10 16:55:16 +0800

    x

--- src/SimplePad.Menu.WinUI/FontMenuItem.xaml.cs
+++ src/SimplePad.Menu.WinUI/FontMenuItem.xaml.cs
@@ -1,11 +1,25 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using SimplePad.Core;
+using SimplePad.Settings;
 
 namespace SimplePad.Menu;
 
 public sealed partial class FontMenuItem : MenuFlyoutItem
 {
+    private readonly SettingsState _settingsState;
+
     public FontMenuItem()
     {
+        _settingsState = ServiceLocator.Current.GetRequiredService<SettingsState>();
+
         InitializeComponent();
+    }
+
+    private void OnClick(object sender, RoutedEventArgs e)
+    {
+        _settingsState.IsVisible = true;
+        _settingsState.IsFontSettingsExpanded = true;
     }
 }
