29c662eda8f3336c56a9bf4e086c8738883c1a28
h82258652 <842053625@qq.com>
2026-06-10 16:39:28 +0800

    x

--- src/SimplePad.Menu.WinUI/DeleteMenuItem.xaml.cs
+++ src/SimplePad.Menu.WinUI/DeleteMenuItem.xaml.cs
@@ -1,11 +1,25 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using SimplePad.Editor;
 
 namespace SimplePad.Menu;
 
 public sealed partial class DeleteMenuItem : MenuFlyoutItem
 {
+    public static readonly DependencyProperty TextBoxProperty = DependencyProperty.Register(
+        nameof(TextBox),
+        typeof(IAppTextBox),
+        typeof(DeleteMenuItem),
+        null);
+
     public DeleteMenuItem()
     {
         InitializeComponent();
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => (IAppTextBox?)GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
     }
 }
