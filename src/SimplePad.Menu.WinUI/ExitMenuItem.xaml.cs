b36e97cdd7a1b6513951295781b87e17d1356fda
h82258652 <842053625@qq.com>
2026-06-10 16:53:26 +0800

    x

--- src/SimplePad.Menu.WinUI/ExitMenuItem.xaml.cs
+++ src/SimplePad.Menu.WinUI/ExitMenuItem.xaml.cs
@@ -1,11 +1,26 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using SimplePad.Core;
+using SimplePad.Windowing;
 
 namespace SimplePad.Menu;
 
 public sealed partial class ExitMenuItem : MenuFlyoutItem
 {
+    private readonly IAppWindowManager _appWindowManager;
+
     public ExitMenuItem()
     {
+        _appWindowManager = ServiceLocator.Current.GetRequiredService<IAppWindowManager>();
+
         InitializeComponent();
+    }
+
+    private async void OnClick(object sender, RoutedEventArgs e)
+    {
+        await Task.WhenAll(_appWindowManager.Instances.Select(_appWindowManager.CloseAsync));
     }
 }
