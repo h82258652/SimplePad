9ce5b1ed42b61fb0987bf1d557888451dcadb91c
h82258652 <842053625@qq.com>
2026-06-10 17:02:05 +0800

    x

--- src/SimplePad.Menu.WinUI/FindPreviousMenuItem.xaml.cs
+++ src/SimplePad.Menu.WinUI/FindPreviousMenuItem.xaml.cs
@@ -13,16 +13,12 @@ using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace SimplePad.Menu;
 
-namespace SimplePad.Menu
+public sealed partial class FindPreviousMenuItem : MenuFlyoutItem
 {
-    public sealed partial class FindPreviousMenuItem : UserControl
+    public FindPreviousMenuItem()
     {
-        public FindPreviousMenuItem()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
