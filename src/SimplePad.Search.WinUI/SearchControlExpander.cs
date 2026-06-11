2d8de7c6e96b24131e9159e1f0945d4e5c3aac70
h82258652 <842053625@qq.com>
2026-06-11 11:11:11 +0800

    x

---
+++ src/SimplePad.Search.WinUI/SearchControlExpander.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml.Controls;
+using SimplePad.Core;
+
+namespace SimplePad.Search;
+
+public sealed partial class SearchControlExpander : Expander
+{
+    private readonly SearchViewState _searchViewState;
+
+    public SearchControlExpander()
+    {
+        _searchViewState = ServiceLocator.Current.GetRequiredService<SearchViewState>();
+
+        UpdateIsExpanded();
+
+        Expanding += OnExpanding;
+        Collapsed += OnCollapsed;
+        _searchViewState.IsReplaceModeChanged += OnSearchViewStateIsReplaceModeChanged;
+    }
+
+    private void OnCollapsed(Expander sender, ExpanderCollapsedEventArgs args)
+    {
+        _searchViewState.IsReplaceMode = IsExpanded;
+    }
+
+    private void OnExpanding(Expander sender, ExpanderExpandingEventArgs args)
+    {
+        _searchViewState.IsReplaceMode = IsExpanded;
+    }
+
+    private void OnSearchViewStateIsReplaceModeChanged(object? sender, bool e)
+    {
+        UpdateIsExpanded();
+    }
+
+    private void UpdateIsExpanded()
+    {
+        IsExpanded = _searchViewState.IsReplaceMode;
+    }
+}
