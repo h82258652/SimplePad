f1f42dd4b538d4944837755203991e3321e8eb2c
h82258652 <842053625@qq.com>
2026-06-11 08:57:34 +0800

    x

---
+++ src/SimplePad.Search.WPF/SearchDownButton.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimplePad.Search
+{
+    /// <summary>
+    /// Interaction logic for SearchDownButton.xaml
+    /// </summary>
+    public partial class SearchDownButton : UserControl
+    {
+        public SearchDownButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
