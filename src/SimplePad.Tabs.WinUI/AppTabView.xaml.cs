8b306c79c39c41dc0085ee6f8cf9a5c3517a9eba
h82258652 <842053625@qq.com>
2026-06-11 08:31:56 +0800

    x

--- src/SimplePad.Tabs.WinUI/AppTabView.xaml.cs
+++ src/SimplePad.Tabs.WinUI/AppTabView.xaml.cs
@@ -1,28 +1,61 @@
+using System;
+using System.Collections.Specialized;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace SimplePad.Tabs;
 
-namespace SimplePad.Tabs
+public sealed partial class AppTabView : UserControl
 {
-    public sealed partial class AppTabView : UserControl
+    public static readonly DependencyProperty TabRootProperty = DependencyProperty.Register(
+        nameof(TabRoot),
+        typeof(TabRoot),
+        typeof(AppTabView),
+        new PropertyMetadata(null, OnTabRootChanged));
+
+    public AppTabView()
     {
-        public AppTabView()
+        InitializeComponent();
+    }
+
+    public TabRoot? TabRoot
+    {
+        get => (TabRoot?)GetValue(TabRootProperty);
+        set => SetValue(TabRootProperty, value);
+    }
+
+    private static void OnTabRootChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        AppTabView self = (AppTabView)d;
+        TabRoot? oldTabRoot = (TabRoot?)e.OldValue;
+        if (oldTabRoot is not null)
         {
-            InitializeComponent();
+            oldTabRoot.Tabs.CollectionChanged -= self.OnTabRootTabsCollectionChanged;
+            oldTabRoot.SelectedTabChanged -= self.OnTabRootSelectedTabChanged;
         }
+
+        TabRoot? newTabRoot = (TabRoot?)e.NewValue;
+        if (newTabRoot is not null)
+        {
+            newTabRoot.Tabs.CollectionChanged += self.OnTabRootTabsCollectionChanged;
+            newTabRoot.SelectedTabChanged += self.OnTabRootSelectedTabChanged;
+        }
+
+        self.UpdateSelectedItem();
+    }
+
+    private void OnTabRootSelectedTabChanged(object? sender, Tab? e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnTabRootTabsCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void UpdateSelectedItem()
+    {
+        throw new NotImplementedException();
     }
 }
--- src/SimplePad.Windowing.WinUI/ShellWindow.xaml.cs
+++ src/SimplePad.Windowing.WinUI/ShellWindow.xaml.cs
@@ -1,24 +1,21 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using SimplePad.Core;
+using SimplePad.Settings;
 
 namespace SimplePad.Windowing;
 
 public sealed partial class ShellWindow : Window
 {
-    public ShellWindow()
+    private readonly IAppWindowManager _appWindowManager;
+    private readonly SettingsState _settingsState;
+
+    public ShellWindow(IAppWindow appWindow)
     {
+        _settingsState = ServiceLocator.Current.GetRequiredService<SettingsState>();
+        _appWindowManager = ServiceLocator.Current.GetRequiredService<IAppWindowManager>();
+
         InitializeComponent();
+        TabView.TabRoot = appWindow.TabRoot;
     }
 }
