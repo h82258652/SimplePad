b17b59f57c74096a9c57b79150c7f3adbe33d1b4
h82258652 <842053625@qq.com>
2026-06-11 08:56:24 +0800

    x

---
+++ src/SimplePad.Search.WPF/ExitFindAndReplaceButton.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+
+namespace SimplePad.Search;
+
+public partial class ExitFindAndReplaceButton : Button
+{
+    private readonly SearchViewState _searchViewState;
+
+    public ExitFindAndReplaceButton()
+    {
+        _searchViewState = ServiceLocator.Current.GetRequiredService<SearchViewState>();
+
+        InitializeComponent();
+    }
+
+    private void OnClick(object sender, RoutedEventArgs e)
+    {
+        _searchViewState.IsVisible = false;
+    }
+}
