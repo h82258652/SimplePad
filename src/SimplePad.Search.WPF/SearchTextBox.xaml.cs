eb05a2de94707abf47be2144dd47e3d6e2f20720
h82258652 <842053625@qq.com>
2026-06-11 08:52:21 +0800

    x

---
+++ src/SimplePad.Search.WPF/SearchTextBox.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using Wpf.Ui.Controls;
+
+namespace SimplePad.Search;
+
+public partial class SearchTextBox : AutoSuggestBox
+{
+    private readonly SearchViewState _searchViewState;
+
+    public SearchTextBox()
+    {
+        _searchViewState = ServiceLocator.Current.GetRequiredService<SearchViewState>();
+
+        InitializeComponent();
+
+        UpdateTextBoxText();
+
+        _searchViewState.SearchTextChanged += OnSearchViewStateSearchTextChanged;
+    }
+
+    private void OnSearchViewStateSearchTextChanged(object? sender, string e)
+    {
+        UpdateTextBoxText();
+    }
+
+    private void OnTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+    {
+        _searchViewState.SearchText = Text;
+    }
+
+    private void UpdateTextBoxText()
+    {
+        Text = _searchViewState.SearchText;
+    }
+}
