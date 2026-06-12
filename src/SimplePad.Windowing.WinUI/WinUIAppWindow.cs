e34608d6b4e777a828f636b62a1019af1011bf64
h82258652 <842053625@qq.com>
2026-06-12 23:22:15 +0800

    x

--- src/SimplePad.Windowing.WinUI/WinUIAppWindow.cs
+++ src/SimplePad.Windowing.WinUI/WinUIAppWindow.cs
@@ -1,22 +1,42 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using SimplePad.Core.Extensions;
 using SimplePad.Tabs;
+using System;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
 
 namespace SimplePad.Windowing;
 
 internal sealed class WinUIAppWindow : IAppWindow
 {
+    private readonly IAppWindowManager _appWindowManager;
+
+    internal WinUIAppWindow(IAppWindowManager appWindowManager)
+    {
+        _appWindowManager = appWindowManager;
+
+        TabRoot.Tabs.CollectionChanged += OnTabsCollectionChanged;
+    }
+
     public TabRoot TabRoot { get; } = new TabRoot();
 
     internal ShellWindow? ShellWindow { get; set; }
 
     public void Execute(Action<IAppWindow> action)
     {
-        throw new NotImplementedException();
+        ShellWindow?.DispatcherQueue.SafeRunAsync(() => action(this));
     }
 
     public Task ShowAsync()
     {
-        throw new NotImplementedException();
+        ShellWindow?.Activate();
+        return Task.CompletedTask;
+    }
+
+    private void OnTabsCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (TabRoot.Tabs.Count == 0)
+        {
+            _appWindowManager.CloseAsync(this);
+        }
     }
 }
