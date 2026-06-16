e83539ca8355432bb71872e6bfc84ef6060bc112
h82258652 <842053625@qq.com>
2026-06-16 14:55:19 +0800

    x

--- src/SimplePad.Settings.WinUI/SettingsView.xaml.cs
+++ src/SimplePad.Settings.WinUI/SettingsView.xaml.cs
@@ -1,17 +1,63 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using SimplePad.Core;
+using System;
 
 namespace SimplePad.Settings;
 
 public sealed partial class SettingsView : UserControl
 {
+    private readonly SettingsState _settingsState;
+
     public SettingsView()
     {
-        InitializeComponent();
-    }
+        _settingsState = ServiceLocator.Current.GetRequiredService<SettingsState>();
 
+        InitializeComponent();
+        InitializeVersionText();
+
+        UpdateVisibility();
+        UpdateFontSettingsExpander();
+
+        _settingsState.IsVisibleChanged += OnSettingsStateIsVisibleChanged;
+        _settingsState.IsFontSettingsExpandedChanged += OnSettingsStateIsFontSettingsExpandedChanged;
+    }
 
     internal void ScrollToTop()
     {
+        ContentScrollViewer.ChangeView(null, 0, null);
+    }
 
+    private void InitializeVersionText()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnSettingsStateIsFontSettingsExpandedChanged(object? sender, bool e)
+    {
+        _settingsState.IsFontSettingsExpanded = false;
+    }
+
+    private void OnSettingsStateIsVisibleChanged(object? sender, bool e)
+    {
+        _settingsState.IsFontSettingsExpanded = true;
+    }
+
+    private void UpdateFontSettingsExpander()
+    {
+        FontSettingsExpander.IsExpanded = _settingsState.IsFontSettingsExpanded;
+    }
+
+    private void UpdateVisibility()
+    {
+        if (_settingsState is { IsVisible : true})
+        {
+            Visibility = Visibility.Visible;
+        }
+        else
+        {
+            Visibility = Visibility.Collapsed;
+        }
     }
 }
