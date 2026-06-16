9afea3e5fac072e8ca156b5df21b4870f09d8020
h82258652 <842053625@qq.com>
2026-06-16 20:02:33 +0800

    x

--- src/SimplePad.Tabs.WinUI/AppTabViewItem.xaml.cs
+++ src/SimplePad.Tabs.WinUI/AppTabViewItem.xaml.cs
@@ -1,6 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using SimplePad.Core;
+using SimplePad.Search;
+using SimplePad.StatusBar;
 using System;
+using System.Threading.Tasks;
 
 namespace SimplePad.Tabs;
 
@@ -12,9 +17,17 @@ public sealed partial class AppTabViewItem : TabViewItem
         typeof(AppTabViewItem),
         new PropertyMetadata(null, OnTabChanged));
 
+    private readonly SearchViewState _searchViewState;
+    private readonly IStatusBarSettings _statusBarSettings;
+
     public AppTabViewItem()
     {
+        _searchViewState = ServiceLocator.Current.GetRequiredService<SearchViewState>();
+        _statusBarSettings = ServiceLocator.Current.GetRequiredService<IStatusBarSettings>();
+
         InitializeComponent();
+
+        RegisterPropertyChangedCallback(IsSelectedProperty, OnIsSelectedChanged);
     }
 
     public Tab? Tab
@@ -29,16 +42,40 @@ public sealed partial class AppTabViewItem : TabViewItem
         Tab? oldTab = (Tab?)e.OldValue;
         if (oldTab is not null)
         {
+            oldTab.TitleChanged -= self.OnTabTitleChanged;
         }
 
         Tab? newTab = (Tab?)e.NewValue;
         if (newTab is not null)
         {
-
+            newTab.TitleChanged += self.OnTabTitleChanged;
         }
 
         self.UpdateHeader();
         self.UpdateTextBox();
+    }
+
+    private void OnTabTitleChanged(object? sender, string e)
+    {
+        UpdateHeader();
+    }
+
+    private async void OnIsSelectedChanged(DependencyObject sender, DependencyProperty dp)
+    {
+        if (IsSelected)
+        {
+            _searchViewState.TextBox = TextBox;
+            await Task.Yield();
+            TextBox.Focus();
+        }
+    }
+
+    private void OnTextBoxTextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (Tab is { } tab)
+        {
+            tab.Content = TextBox.Text;
+        }
     }
 
     private void UpdateHeader()
@@ -49,13 +86,5 @@ public sealed partial class AppTabViewItem : TabViewItem
     private void UpdateTextBox()
     {
         TextBox.Text = Tab?.Content ?? string.Empty;
-    }
-
-    private void OnTextBoxTextChanged(object sender, TextChangedEventArgs e)
-    {
-        if (Tab is { } tab)
-        {
-            tab.Content = TextBox.Text;
-        }
     }
 }
