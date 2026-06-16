a801e3e8e70d8d13ff7f719b04c98c3ec6946a64
h82258652 <842053625@qq.com>
2026-06-16 17:33:08 +0800

    x

--- src/SimplePad.Menu.Avalonia/NewTabMenuItem.axaml.cs
+++ src/SimplePad.Menu.Avalonia/NewTabMenuItem.axaml.cs
@@ -1,13 +1,27 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using SimplePad.Tabs;
 
 namespace SimplePad.Menu;
 
-public partial class NewTabMenuItem : UserControl
+public sealed partial class NewTabMenuItem : MenuItem
 {
+    public static readonly StyledProperty<TabRoot?> TabRootProperty = AvaloniaProperty.Register<NewTabMenuItem, TabRoot?>(nameof(TabRoot));
+
     public NewTabMenuItem()
     {
         InitializeComponent();
+    }
+
+    public TabRoot? TabRoot
+    {
+        get => GetValue(TabRootProperty);
+        set => SetValue(TabRootProperty, value);
+    }
+
+    private void OnClick(object? sender, RoutedEventArgs e)
+    {
+        TabRoot?.AddBlankTab();
     }
 }
