dfcdb8d7d2db7b8b69b3f9326951d03f5bc11e82
h82258652 <842053625@qq.com>
2026-06-17 15:46:40 +0800

    x

--- src/SimplePad.Menu.Avalonia/FindPreviousMenuItem.axaml.cs
+++ src/SimplePad.Menu.Avalonia/FindPreviousMenuItem.axaml.cs
@@ -4,7 +4,7 @@ using Avalonia.Markup.Xaml;
 
 namespace SimplePad.Menu;
 
-public partial class FindPreviousMenuItem : UserControl
+public partial class FindPreviousMenuItem : MenuItem
 {
     public FindPreviousMenuItem()
     {
