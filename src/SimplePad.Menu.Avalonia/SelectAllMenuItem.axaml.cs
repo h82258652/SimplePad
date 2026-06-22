54195578e21e62d4e83cf84fd2a35fea719d06de
h82258652 <842053625@qq.com>
2026-06-22 13:42:43 +0800

    x

--- src/SimplePad.Menu.Avalonia/SelectAllMenuItem.axaml.cs
+++ src/SimplePad.Menu.Avalonia/SelectAllMenuItem.axaml.cs
@@ -1,13 +1,28 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using SimplePad.Editor;
 
 namespace SimplePad.Menu;
 
 public partial class SelectAllMenuItem : MenuItem
 {
+    public static readonly StyledProperty<IAppTextBox?> TextBoxProperty =
+        AvaloniaProperty.Register<SelectAllMenuItem, IAppTextBox?>(nameof(TextBox));
+
     public SelectAllMenuItem()
     {
         InitializeComponent();
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
+    }
+
+    private void OnClick(object? sender, RoutedEventArgs e)
+    {
+        TextBox?.SelectAll();
     }
 }
