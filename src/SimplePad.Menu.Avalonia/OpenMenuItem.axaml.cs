e915e5be049fab1120cb4456b64ad4dbaea96f3c
h82258652 <842053625@qq.com>
2026-06-25 12:01:28 +0800

    x

--- src/SimplePad.Menu.Avalonia/OpenMenuItem.axaml.cs
+++ src/SimplePad.Menu.Avalonia/OpenMenuItem.axaml.cs
@@ -1,10 +1,8 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 
 namespace SimplePad.Menu;
 
-public partial class OpenMenuItem : UserControl
+public partial class OpenMenuItem : MenuItem
 {
     public OpenMenuItem()
     {
