07d729aa90d6cd051a0152029ae3f93db3127bef
h82258652 <842053625@qq.com>
2026-06-25 12:07:54 +0800

    x

--- src/SimplePad.Menu.Avalonia/PasteMenuItem.axaml.cs
+++ src/SimplePad.Menu.Avalonia/PasteMenuItem.axaml.cs
@@ -1,11 +1,22 @@
+using Avalonia;
 using Avalonia.Controls;
+using SimplePad.Editor;
 
 namespace SimplePad.Menu;
 
 public partial class PasteMenuItem : MenuItem
 {
+    public static readonly StyledProperty<IAppTextBox?> TextBoxProperty =
+        AvaloniaProperty.Register<PasteMenuItem, IAppTextBox?>(nameof(TextBox));
+
     public PasteMenuItem()
     {
         InitializeComponent();
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
     }
 }
