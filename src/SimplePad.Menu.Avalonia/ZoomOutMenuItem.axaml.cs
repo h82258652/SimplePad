7ab5546f77e643695a159f805c25fd04cde5a0c9
h82258652 <842053625@qq.com>
2026-06-25 11:38:49 +0800

    x

--- src/SimplePad.Menu.Avalonia/ZoomOutMenuItem.axaml.cs
+++ src/SimplePad.Menu.Avalonia/ZoomOutMenuItem.axaml.cs
@@ -1,13 +1,52 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using SimplePad.Editor;
+using System;
+using System.Threading.Tasks;
 
 namespace SimplePad.Menu;
 
-public partial class ZoomOutMenuItem : UserControl
+public partial class ZoomOutMenuItem : MenuItem
 {
+    public static readonly StyledProperty<IAppTextBox?> TextBoxProperty =
+        AvaloniaProperty.Register<ZoomOutMenuItem, IAppTextBox?>(nameof(TextBox));
+
+    private readonly EditorZoomState _editorZoomState;
+
     public ZoomOutMenuItem()
     {
+        _editorZoomState = ServiceLocator.Current.GetRequiredService<EditorZoomState>();
+
         InitializeComponent();
+
+        UpdateIsEnabled();
+
+        _editorZoomState.CanZoomOutChanged += OnEditorZoomStateCanZoomOutChanged;
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
+    }
+
+    private async void OnClick(object? sender, RoutedEventArgs e)
+    {
+        _editorZoomState.ZoomOut();
+        await Task.Yield();
+        TextBox?.Focus();
+    }
+
+    private void OnEditorZoomStateCanZoomOutChanged(object? sender, bool e)
+    {
+        UpdateIsEnabled();
+    }
+
+    private void UpdateIsEnabled()
+    {
+        IsEnabled = _editorZoomState.CanZoomOut;
     }
 }
