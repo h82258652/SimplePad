a89d31b0fc7260e2e55ffb36218f655abda84e1f
h82258652 <842053625@qq.com>
2026-06-25 12:23:04 +0800

    x

---
+++ src/SimplePad.Search.Avalonia/ReplaceAllButton.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SimplePad.Search;
+
+public partial class ReplaceAllButton : UserControl
+{
+    public ReplaceAllButton()
+    {
+        InitializeComponent();
+    }
+}
