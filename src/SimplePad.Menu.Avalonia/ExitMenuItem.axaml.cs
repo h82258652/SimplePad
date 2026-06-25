2ca79f6fe87355127685edca40c204f22e35084a
h82258652 <842053625@qq.com>
2026-06-25 11:58:54 +0800

    x

--- src/SimplePad.Menu.Avalonia/ExitMenuItem.axaml.cs
+++ src/SimplePad.Menu.Avalonia/ExitMenuItem.axaml.cs
@@ -1,13 +1,27 @@
-using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using SimplePad.Windowing;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SimplePad.Menu;
 
-public partial class ExitMenuItem : UserControl
+public partial class ExitMenuItem : MenuItem
 {
+    private readonly IAppWindowManager _appWindowManager;
+
     public ExitMenuItem()
     {
+        _appWindowManager = ServiceLocator.Current.GetRequiredService<IAppWindowManager>();
+
         InitializeComponent();
+    }
+
+    private async void OnClick(object? sender, RoutedEventArgs e)
+    {
+        await Task.WhenAll(_appWindowManager.Instances.Select(_appWindowManager.CloseAsync));
     }
 }
