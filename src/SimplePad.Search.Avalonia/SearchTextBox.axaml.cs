3279e0cb79d0b5d426944c6ece8b9f54c64f3718
h82258652 <842053625@qq.com>
2026-06-25 12:22:06 +0800

    x

---
+++ src/SimplePad.Search.Avalonia/SearchTextBox.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SimplePad.Search;
+
+public partial class SearchTextBox : UserControl
+{
+    public SearchTextBox()
+    {
+        InitializeComponent();
+    }
+}
--- src/SimplePad.Search.UWP/SearchTextBox.xaml.cs
+++ src/SimplePad.Search.UWP/SearchTextBox.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using SimplePad.Core;
 using Windows.UI.Xaml.Controls;
 
