6ea9fa4a99b0cfa437e85ff99ef1d0f939c1ef27
h82258652 <842053625@qq.com>
2026-06-25 11:34:51 +0800

    x

--- src/SimplePad.Menu.Avalonia/ZoomInMenuItem.axaml.cs
+++ src/SimplePad.Menu.Avalonia/ZoomInMenuItem.axaml.cs
@@ -1,13 +1,52 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePad.Core;
+using SimplePad.Editor;
+using System.Threading.Tasks;
 
 namespace SimplePad.Menu;
 
-public partial class ZoomInMenuItem : UserControl
+public partial class ZoomInMenuItem : MenuItem
 {
+    public static readonly StyledProperty<IAppTextBox?> TextBoxProperty =
+        AvaloniaProperty.Register<ZoomInMenuItem, IAppTextBox?>(nameof(TextBox));
+
+    private readonly EditorZoomState _editorZoomState;
+
     public ZoomInMenuItem()
     {
+        _editorZoomState = ServiceLocator.Current.GetRequiredService<EditorZoomState>();
+
         InitializeComponent();
+
+        UpdateIsEnabled();
+
+        _editorZoomState.CanZoomInChanged += OnEditorZoomStateCanZoomInChanged;
+    }
+
+    public IAppTextBox? TextBox
+    {
+        get => GetValue(TextBoxProperty);
+        set => SetValue(TextBoxProperty, value);
+    }
+
+    private async void OnClick(object? sender, RoutedEventArgs e)
+    {
+        _editorZoomState.ZoomIn();
+        await Task.Yield();
+        TextBox?.Focus();
+    }
+
+    private void OnEditorZoomStateCanZoomInChanged(object? sender, bool e)
+    {
+        UpdateIsEnabled();
+    }
+
+    private void UpdateIsEnabled()
+    {
+        IsEnabled = _editorZoomState.CanZoomIn;
     }
 }
