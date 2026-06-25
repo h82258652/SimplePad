a841dc0a37a6eceb146d4bbbb89e74a2e5f60f79
h82258652 <842053625@qq.com>
2026-06-25 12:02:48 +0800

    x

--- src/SimplePad.Menu.Avalonia/PasteMenuItem.axaml.cs
+++ src/SimplePad.Menu.Avalonia/PasteMenuItem.axaml.cs
@@ -1,10 +1,8 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 
 namespace SimplePad.Menu;
 
-public partial class PasteMenuItem : UserControl
+public partial class PasteMenuItem : MenuItem
 {
     public PasteMenuItem()
     {
