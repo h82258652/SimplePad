456638757bf73ceb3c751deae74a73735e9931a2
h82258652 <842053625@qq.com>
2026-07-02 17:16:45 +0800

    x

---
+++ src/SimplePad.Tabs.WinUI/InversedVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace SimplePad.Tabs;
+
+internal sealed partial class InversedVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        Visibility visibility = (Visibility)value;
+        return visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        Visibility visibility = (Visibility)value;
+        return visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+    }
+}
