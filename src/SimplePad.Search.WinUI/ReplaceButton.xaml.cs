5918ad4e9202396251c44be66b019cb2b80bf200
h82258652 <842053625@qq.com>
2026-08-06 16:20:43 +0800

    x

---
+++ src/SimplePad.Search.WinUI/ReplaceButton.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace SimplePad.Search;
+
+public sealed partial class ReplaceButton : Button
+{
+    public ReplaceButton()
+    {
+        InitializeComponent();
+    }
+}
