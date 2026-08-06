d0d7fa6b90bf50b6b2839a1a1265abc551ecd4d1
h82258652 <842053625@qq.com>
2026-08-06 15:32:49 +0800

    x

---
+++ src/SimplePad.Search.Avalonia/MoveDownMenuItem.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SimplePad.Search;
+
+public partial class MoveDownMenuItem : UserControl
+{
+    public MoveDownMenuItem()
+    {
+        InitializeComponent();
+    }
+}
